ed15395114717d57e2aa29bde1aa1dec5cedc21e
stone-liu3432 <liushuang3432@163.com>
2019-12-07 11:06:56 +0800

    添加全局less变量

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    productionSourceMap: false,
+    pluginOptions: {
+        "style-resources-loader": {
+            preProcessor: "less",
+            patterns: [
+                path.resolve(__dirname, "./src/config/global-style.less")
+            ]
+        }
+    }
+}
