83975af92dccf590060dc8697df7a484df09ea65
stone-liu3432 <liushuang3432@163.com>
2019-12-13 14:37:42 +0800

    去除eslint代码检测

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+"use strict";
+const path = require("path");
 
 module.exports = {
     productionSourceMap: false,
@@ -9,5 +10,16 @@ module.exports = {
                 path.resolve(__dirname, "./src/config/global-style.less")
             ]
         }
+    },
+    chainWebpack: config => {
+        const eslint = config.module.rule("eslint");
+        eslint.uses.clear();
     }
-}
+};
+
+/**
+ * 想了解 webpack当前的全部配置项，
+ * 可在当前项目目录下，命令行输入 `vue inspect > output.js`，从导出的文件中查看
+ * output.js 可任意命名
+ * 当前关闭 eslint主要原因是vue template中的长字符串频繁报错
+ */
