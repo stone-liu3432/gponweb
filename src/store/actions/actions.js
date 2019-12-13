5aeacdc2cd75f9b9ac425aaeddf1a5e7dcf68058
stone-liu3432 <liushuang3432@163.com>
2019-12-13 15:16:22 +0800

    添加获取system,pon,port的action，并根据system和port生成端口名称

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,19 +1,99 @@
 import axios from "@/config/axios";
-import { isPlainObject, isArray } from "@/utils/common";
+import {
+    isEmptyObject,
+    isPlainObject,
+    isArray,
+    isDef,
+    isUndef
+} from "@/utils/common";
+
+// portName生成，根据端口号和端口数量生成显示在界面上的 name
+const createPortName = state => {
+    if (
+        !isEmptyObject(state.system) &&
+        isArray(state.port) &&
+        state.port.length
+    ) {
+        const { ponports, geports, xgeports } = state.system;
+        return state.port.reduce((prev, item) => {
+            const id = item.port_id;
+            if (isUndef(prev[id])) {
+                prev[id] = {};
+            }
+            const o = prev[id];
+            let name =
+                id <= ponports
+                    ? `PON${id < 10 ? "0" + id : id}`
+                    : id <= ponports + geports
+                    ? `GE${
+                          id - ponports < 10
+                              ? "0" + (id - ponports)
+                              : id - ponports
+                      }`
+                    : xgeports
+                    ? `XGE${
+                          id - ponports - geports < 10
+                              ? "0" + (id - ponports - geports)
+                              : id - ponports - geports
+                      }`
+                    : "";
+            if (item.link_aggregation) {
+                name += `(LAG${item.link_aggregation})`;
+            }
+            o.port_id = id;
+            o.name = name;
+            return prev;
+        }, {});
+    }
+};
 
 const actions = {
-    getLang({ commit }) {
+    getSystemInfo({ commit, state }) {
         axios
-            .get("/system_lang")
+            .get("/board?info=system")
             .then(res => {
                 if (res.data.code === 1) {
                     if (isPlainObject(res.data.data)) {
-                        commit("updateLang", res.data.data.lang || "en");
-                        sessionStorage.setItem(
-                            "lang",
-                            res.data.data.lang || "en"
-                        );
+                        commit("updateSystem", res.data.data);
+                        const _portnames = createPortName(state);
+                        if (isDef(_portnames)) {
+                            commit("updatePortNames", _portnames);
+                        }
                     }
+                } else {
+                    commit("updateSystem", {});
+                }
+            })
+            .catch(err => {});
+    },
+    getPon({ commit }) {
+        axios
+            .get("/board?info=pon")
+            .then(res => {
+                if (res.data.code === 1) {
+                    if (isArray(res.data.data)) {
+                        commit("updatePon", res.data.data);
+                    }
+                } else {
+                    commit("updatePon", []);
+                }
+            })
+            .catch(err => {});
+    },
+    getPort({ commit, state }) {
+        axios
+            .get("/switch_port?form=portlist_info")
+            .then(res => {
+                if (res.data.code === 1) {
+                    if (isArray(res.data.data)) {
+                        commit("updatePort", res.data.data);
+                        const _portnames = createPortName(state);
+                        if (isDef(_portnames)) {
+                            commit("updatePortNames", _portnames);
+                        }
+                    }
+                } else {
+                    commit("updatePort", []);
                 }
             })
             .catch(err => {});
