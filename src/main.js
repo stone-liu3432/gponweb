a9cb7f7c28ab3ae5521343c988a06aee4e685547
stone-liu3432 <liushuang3432@163.com>
2019-12-13 15:23:26 +0800

    修改i18n注入方式

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import router from "./router";
 import store from "./store";
 import baseConfig from "@/config/baseConfig";
 
-baseConfig(Vue);
+const { i18n } = baseConfig(Vue);
 
 new Vue({
     router,
+    i18n,
     store,
     render: h => h(hsgq)
 }).$mount("#app");
