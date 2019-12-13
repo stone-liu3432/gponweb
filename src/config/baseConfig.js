8ff07231298269f060fdf28ca69bbe6acc3ebeba
stone-liu3432 <liushuang3432@163.com>
2019-12-13 15:07:22 +0800

    替换$confirm的按钮默认文本，修改i18n引用方式

--- src/config/baseConfig.js
+++ src/config/baseConfig.js
@@ -4,6 +4,8 @@ import locale from "element-ui/lib/locale";
 import enLocale from "element-ui/lib/locale/lang/en";
 import zhLocale from "element-ui/lib/locale/lang/zh-CN";
 import axios from "@/config/axios";
+import { isFunction, isPlainObject } from "@/utils/common";
+import store from "../store";
 
 const __DEV__ = process.env.NODE_ENV === "development";
 
@@ -11,8 +13,36 @@ if (__DEV__) {
     require("./mock.js");
 }
 
-function baseConfig(Vue) {
-    // to do
+const overrideMethods = Vue => {
+    const cfm = Vue.prototype.$confirm;
+    const msg = Vue.prototype.$message;
+    // 替换默认的 确认/取消 按钮的文本
+    Vue.prototype.$confirm = (content, title, options) => {
+        if (isPlainObject(title)) {
+            options = title;
+            title = "";
+        }
+        const {
+            getters: { $lang }
+        } = store;
+        if (isFunction($lang)) {
+            return cfm(
+                content,
+                title,
+                Object.assign(
+                    {
+                        cancelButtonText: $lang("cancel"),
+                        confirmButtonText: $lang("apply")
+                    },
+                    options
+                )
+            );
+        }
+        return cfm(content, title, options);
+    };
+};
+
+const baseConfig = Vue => {
     Vue.config.productionTip = false;
 
     Vue.use(VueI18n);
@@ -24,10 +54,13 @@ function baseConfig(Vue) {
         }
     });
     locale.i18n((key, value) => i18n.t(key, value));
-    Vue.use(ElementUI, { size: "small", zIndex: 1000, locale });
+    Vue.use(ElementUI, { size: "small", zIndex: 1000 });
 
     Vue.prototype.$http = axios;
     Vue.prototype.$is_dev = _ => __DEV__;
-}
+
+    overrideMethods(Vue);
+    return { i18n };
+};
 
 export default baseConfig;
