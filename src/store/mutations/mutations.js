025e1963afaa1332ab63517ba6c899c9b859da5f
stone-liu3432 <liushuang3432@163.com>
2019-12-13 15:18:44 +0800

    添加更新system,pon,port,portName的mutation

--- src/store/mutations/mutations.js
+++ src/store/mutations/mutations.js
@@ -1,6 +1,19 @@
 const mutations = {
     updateLang(state, data) {
         state.lang = data;
+        sessionStorage.setItem("lang", data);
+    },
+    updateSystem(state, data) {
+        state.system = data;
+    },
+    updatePon(state, data) {
+        state.pon = data;
+    },
+    updatePort(state, data) {
+        state.port = data;
+    },
+    updatePortNames(state, data) {
+        state.portName = data;
     }
 };
 
