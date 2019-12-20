5cb93a30963b10e5781ec77886525a30f91b15e0
stone-liu3432 <liushuang3432@163.com>
2019-12-20 18:00:44 +0800

    完善advanced功能，添加引用

--- src/views/page/advancedSetting.vue
+++ src/views/page/advancedSetting.vue
@@ -1,12 +1,22 @@
 <template>
-    <div>advanced setting</div>
+    <el-container>
+        <el-aside width="220px">
+            <adv-aside></adv-aside>
+        </el-aside>
+        <el-main>
+            <adv-content></adv-content>
+        </el-main>
+    </el-container>
 </template>
 
 <script>
+import advContent from "@/views/advSetting/content";
+import advAside from "@/views/advSetting/aside";
 export default {
-    name: "advancedSetting"
+    name: "advancedSetting",
+    components: { advContent, advAside }
 };
 </script>
 
-<style>
+<style lane="less" scoped>
 </style>
