a0a7b5fe6fe57dc8925720cc4627ca0039cd9d9b
stone-liu3432 <liushuang3432@163.com>
2019-12-20 17:59:51 +0800

    添加高级菜单下的内容区

---
+++ src/views/advSetting/content.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "adv-main"
+};
+</script>
+
+<style lang="less" scoped>
+</style>
