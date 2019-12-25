c3de46ad8c89b5e837a2f840ffdc684014fd9c6d
stone-liu3432 <liushuang3432@163.com>
2019-12-25 14:40:22 +0800

    添加刷新和下载按钮

--- src/views/advSetting/system/alarm.vue
+++ src/views/advSetting/system/alarm.vue
@@ -1,23 +1,37 @@
 <template>
     <div class="alarm">
         <page-header :title="$lang('alarm')" type="none"></page-header>
+        <div>
+            <span>{{ $lang('alarm_info') }}</span>
+            <el-button
+                type="primary"
+                style="margin-left: 30px;"
+                @click="refreshData"
+            >{{ $lang('refresh') }}</el-button>
+            <el-button
+                type="primary"
+                style="margin-left: 30px;"
+                @click="downloadAlarm"
+            >{{ $lang('download') }}</el-button>
+        </div>
         <ul>
             <template v-for="item in alarmList">
                 <li style="line-height: 30px;">{{ item }}</li>
             </template>
         </ul>
+        <el-backtop :target="backTopTarget"></el-backtop>
     </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
-import { isArray } from "@/utils/common";
+import { isArray, debounce, isDef } from "@/utils/common";
 export default {
     name: "sysAlarm",
     computed: {
         ...mapGetters(["$lang"])
     },
-    inject: ["updateAdvMainScrollbar"],
+    inject: ["updateAdvMainScrollbar", "backTopTarget"],
     updated() {
         this.$nextTick(_ => {
             this.updateAdvMainScrollbar();
@@ -44,6 +58,43 @@ export default {
                     }
                 })
                 .catch(err => {});
+        },
+        refreshData() {
+            debounce(this.getData, 1000, this);
+        },
+        downloadAlarm() {
+            this.$confirm(this.$lang("if_sure", "download"))
+                .then(_ => {
+                    this.$http
+                        .get("/alarm?form=download")
+                        .then(res => {
+                            if (res.data.code === 1) {
+                                if (isDef(res.data.data)) {
+                                    const filename = res.data.data.filename;
+                                    if (isDef(filename)) {
+                                        try {
+                                            var a = document.createElement("a");
+                                            a.href = "/" + filename;
+                                            a.setAttribute(
+                                                "download",
+                                                filename
+                                            );
+                                            a.style.display = "none";
+                                            document.body.appendChild(a);
+                                            a.click();
+                                            document.body.removeChild(a);
+                                        } catch (e) {}
+                                    }
+                                }
+                            } else {
+                                this.$message.error(
+                                    `(${res.data.code}) ${res.data.message}`
+                                );
+                            }
+                        })
+                        .catch(err => {});
+                })
+                .catch(_ => {});
         }
     }
 };
@@ -52,5 +103,11 @@ export default {
 <style lang="less" scoped>
 li {
     .base-font-style;
+}
+div {
+    margin: 10px;
+    span {
+        color: @titleColor;
+    }
 }
 </style>
