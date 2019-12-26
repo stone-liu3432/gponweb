d0ea90b07a27d49ae262d4082fadbaed1d5e9093
stone-liu3432 <liushuang3432@163.com>
2019-12-26 13:41:46 +0800

    添加perf info页面

---
+++ src/views/advSetting/switch/perf/detail.vue
@@ -0,0 +1,28 @@
+<template>
+    <el-row style="text-transform: capitalize;">
+        <template v-for="(item, key) in data">
+            <template v-if="key !== 'port_id'">
+                <el-col :span="6" style="padding: 6px;">{{ key.replace(/_/g, ' ') }}:</el-col>
+                <el-col :span="6" style="padding: 6px;">{{ item }}</el-col>
+            </template>
+        </template>
+    </el-row>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+    name: "perfDetail",
+    props: {
+        data: {
+            type: Object
+        }
+    },
+    computed: {
+        ...mapGetters(["$lang"])
+    }
+};
+</script>
+
+<style lang='less' scoped>
+</style>
