0396fd913d631d2404b415db2c7d0bdbbd2f0c22
stone-liu3432 <liushuang3432@163.com>
2019-12-26 13:43:03 +0800

    添加port config页面

---
+++ src/views/advSetting/switch/portConfig.vue
@@ -0,0 +1,287 @@
+<template>
+    <div>
+        <page-header :title="$lang('port_cfg')" type="port" @port-change="portChange"></page-header>
+        <el-row :gutter="30">
+            <el-col :span="8">
+                <el-card shadow="never">
+                    <template slot="header">
+                        <span>{{ $lang('sw_port_cfg') }}</span>
+                        <el-button
+                            type="text"
+                            style="float: right; padding: 3px 0;"
+                            @click="openDialog('sw_port_cfg')"
+                        >{{ $lang('config') }}</el-button>
+                    </template>
+                    <el-form label-width="140px" label-position="left">
+                        <el-form-item
+                            :label="$lang('admin_status')"
+                        >{{ basic.admin_status ? $lang('enable') : $lang('disable') }}</el-form-item>
+                        <el-form-item
+                            :label="$lang('link_status')"
+                        >{{ basic.link_status ? $lang('link_up') : $lang('link_down') }}</el-form-item>
+                        <el-form-item
+                            :label="$lang('auto_neg')"
+                        >{{ basic.auto_neg ? $lang('enable') : $lang('disable') }}</el-form-item>
+                        <el-form-item :label="$lang('speed')">{{ basic.speed}}</el-form-item>
+                        <el-form-item :label="$lang('duplex')">{{ basic.duplex ? 'full' : 'half' }}</el-form-item>
+                        <el-form-item
+                            :label="$lang('flow_ctrl')"
+                        >{{ basic.flow_ctrl ? $lang('enable') : $lang('disable') }}</el-form-item>
+                        <el-form-item :label="$lang('mtu')">{{ basic.mtu }}</el-form-item>
+                        <el-form-item :label="$lang('media')">{{ $lang(basic.media) }}</el-form-item>
+                        <el-form-item :label="$lang('erate')">{{ basic.erate }}</el-form-item>
+                        <el-form-item :label="$lang('irate')">{{ basic.irate }}</el-form-item>
+                        <el-form-item :label="$lang('pvid')">{{ basic.pvid }}</el-form-item>
+                    </el-form>
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card shadow="never">
+                    <template slot="header">
+                        <span>{{ $lang('stormctrl') }}</span>
+                        <el-button
+                            type="text"
+                            style="float: right; padding: 3px 0;"
+                            @click="openDialog('stormctrl')"
+                        >{{ $lang('config') }}</el-button>
+                    </template>
+                    <el-form label-width="140px" label-position="left">
+                        <el-form-item :label="$lang('broadcast')">{{ storm.broadcast }}</el-form-item>
+                        <el-form-item :label="$lang('multicast')">{{ storm.multicast }}</el-form-item>
+                        <el-form-item :label="$lang('unicast')">{{ storm.unicast }}</el-form-item>
+                    </el-form>
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card shadow="never">
+                    <template slot="header">
+                        <span>{{ $lang('mirror') }}</span>
+                        <el-button
+                            type="text"
+                            style="float: right; padding: 3px 0;"
+                            @click="openDialog('mirror')"
+                        >{{ $lang('config') }}</el-button>
+                    </template>
+                    <el-form label-width="140px" label-position="left">
+                        <el-form-item
+                            :label="$lang('dst_port')"
+                        >{{ getPortName(mirror.dst_port) || ' - ' }}</el-form-item>
+                        <el-form-item :label="$lang('type')">{{ types[mirror.type] }}</el-form-item>
+                    </el-form>
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-dialog :visible.sync="dialogVisible" append-to-body>
+            <template slot="title">{{ $lang(dialogType) }}</template>
+            <port-config-form :type="dialogType" :data="dialogData" ref="port-config-form"></port-config-form>
+            <span slot="footer">
+                <el-button @click="closeDialog">{{ $lang('cancel') }}</el-button>
+                <el-button
+                    type="primary"
+                    @click="submitForm('port-config-form')"
+                >{{ $lang('apply') }}</el-button>
+            </span>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+import { isDef, isFunction } from "@/utils/common";
+import portConfigForm from "./portConfig/form";
+import postData from "@/mixin/postData";
+export default {
+    name: "portConfig",
+    mixins: [postData],
+    components: { portConfigForm },
+    computed: {
+        ...mapGetters(["$lang", "getPortName"])
+    },
+    data() {
+        return {
+            port_id: 0,
+            types: [" - ", "ingress", "egress", "all"],
+            basic: {},
+            storm: {},
+            mirror: {},
+            dialogData: {},
+            dialogType: "",
+            dialogVisible: false
+        };
+    },
+    created() {},
+    methods: {
+        ...mapActions(["getPort"]),
+        portChange(port_id) {
+            this.port_id = port_id;
+            this.getBasic(port_id);
+            this.getStorm(port_id);
+            this.getMirror(port_id);
+        },
+        getBasic(port_id) {
+            this.basic = {};
+            this.$http
+                .get("/switch_port?form=port_info&port_id=" + port_id)
+                .then(res => {
+                    if (res.data.code === 1) {
+                        if (isDef(res.data.data)) {
+                            this.basic = res.data.data;
+                        }
+                    }
+                })
+                .catch(err => {});
+        },
+        getStorm(port_id) {
+            this.storm = {};
+            this.$http
+                .get("/switch_port?form=stormctrl&port_id=" + port_id)
+                .then(res => {
+                    if (res.data.code === 1) {
+                        if (isDef(res.data.data)) {
+                            this.storm = res.data.data;
+                        }
+                    }
+                })
+                .catch(err => {});
+        },
+        getMirror(port_id) {
+            this.mirror = {};
+            this.$http
+                .get("/switch_port?form=mirror&port_id=" + port_id)
+                .then(res => {
+                    if (res.data.code === 1) {
+                        if (isDef(res.data.data)) {
+                            this.mirror = res.data.data;
+                        }
+                    }
+                })
+                .catch(err => {});
+        },
+        openDialog(type) {
+            const data = {
+                sw_port_cfg: this.basic,
+                stormctrl: this.storm,
+                mirror: this.mirror
+            };
+            this.dialogData = data[type];
+            this.dialogType = type;
+            this.dialogVisible = true;
+            this.$nextTick(_ => {
+                this.$refs["port-config-form"].init();
+            });
+        },
+        submitForm(formName) {
+            this.$refs["port-config-form"].validate(data => {
+                if (data) {
+                    this.submitAction(data, this.dialogType);
+                }
+            });
+        },
+        submitAction(data, type) {
+            const ACTIONS = {
+                sw_port_cfg(data, type) {
+                    const flags = {
+                        admin_status: 0x01,
+                        auto_neg: 0x02,
+                        duplex: 0x04,
+                        flow_ctrl: 0x18,
+                        speed: 0x40,
+                        mtu: 0x80,
+                        erate: 0x100,
+                        irate: 0x200,
+                        pvid: 0x400
+                    };
+                    const flag = this.computedFlag(flags, data, this.basic);
+                    if (flag === 0) {
+                        this.$message.error(this.$lang("modify_tips"));
+                        return;
+                    }
+                    return {
+                        url: "/switch_port?form=port_info",
+                        data: {
+                            method: "set",
+                            param: {
+                                port_id: this.port_id,
+                                flags: flag,
+                                admin_status: data.admin_status,
+                                auto_neg: data.auto_neg,
+                                speed: data.speed,
+                                duplex: data.duplex,
+                                flow_ctrl: data.flow_ctrl,
+                                mtu: data.mtu,
+                                erate: data.erate,
+                                irate: data.irate,
+                                pvid: data.pvid
+                            }
+                        }
+                    };
+                },
+                stormctrl(data, type) {
+                    const flags = {
+                        broadcast: 0x04,
+                        multicast: 0x02,
+                        unicast: 0x01
+                    };
+                    const flag = this.computedFlag(flags, data, this.storm);
+                    if (flag === 0) {
+                        this.$message.error(this.$lang("modify_tips"));
+                        return;
+                    }
+                    return {
+                        url: "/switch_port?form=stormctrl",
+                        data: {
+                            method: "set",
+                            param: {
+                                port_id: this.port_id,
+                                flags: flag,
+                                broadcast: data.broadcast,
+                                multicast: data.multicast,
+                                unicast: data.unicast
+                            }
+                        }
+                    };
+                },
+                mirror(data) {
+                    return {
+                        url: "/switch_port?form=mirror",
+                        data: {
+                            method: "set",
+                            param: {
+                                src_port: this.port_id,
+                                dst_port: data.dst_port,
+                                type: data.type
+                            }
+                        }
+                    };
+                }
+            };
+            if (isFunction(ACTIONS[type])) {
+                const result = ACTIONS[type].call(this, data, type);
+                if (result) {
+                    const { data: post_param, url } = result;
+                    console.log("post params -> ", post_param, ",url -> ", url);
+                    this.postData(url, post_param)
+                        .then(_ => {})
+                        .catch(_ => {});
+                }
+                this.closeDialog();
+            }
+        },
+        computedFlag(flags, data, baseData) {
+            let flag = 0;
+            Object.keys(flags).forEach(key => {
+                if (data[key] !== baseData[key]) {
+                    flag |= flags[key];
+                }
+            });
+            return flag;
+        },
+        closeDialog() {
+            this.dialogVisible = false;
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/advSetting/switch/portConfig/form.vue
@@ -0,0 +1,290 @@
+<template>
+    <el-form label-width="200px" :model="formData" :rules="rules" ref="port-config-form">
+        <template v-if="type === 'sw_port_cfg'">
+            <el-form-item :label="$lang('admin_status')" prop="admin_status">
+                <el-select
+                    v-model.number="formData.admin_status"
+                    :disabled="disabled('admin_status',data.port_id)"
+                >
+                    <el-option :label="$lang('disable')" :value="0"></el-option>
+                    <el-option :label="$lang('enable')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('link_status')" prop="link_status">
+                <el-select v-model.number="formData.link_status" disabled>
+                    <el-option :label="$lang('link_down')" :value="0"></el-option>
+                    <el-option :label="$lang('link_up')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('auto_neg')" prop="auto_neg">
+                <el-select v-model.number="formData.auto_neg">
+                    <el-option :label="$lang('disable')" :value="0"></el-option>
+                    <el-option :label="$lang('enable')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('speed')" prop="speed">
+                <el-select v-model="formData.speed" :disabled="disabled('speed',data.port_id)">
+                    <el-option value="10M"></el-option>
+                    <el-option value="100M"></el-option>
+                    <el-option value="1000M"></el-option>
+                    <el-option value="10000M"></el-option>
+                    <el-option value="auto" label="Auto"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('duplex')" prop="duplex">
+                <el-select
+                    v-model.number="formData.duplex"
+                    :disabled="disabled('duplex',data.port_id)"
+                >
+                    <el-option :label="$lang('half')" :value="0"></el-option>
+                    <el-option :label="$lang('full')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('flow_ctrl')" prop="flow_ctrl">
+                <el-select v-model.number="formData.flow_ctrl">
+                    <el-option :label="$lang('disable')" :value="0"></el-option>
+                    <el-option :label="$lang('enable')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('mtu')" prop="mtu">
+                <el-input v-model="formData.mtu" style="width: 200px;"></el-input>
+            </el-form-item>
+            <el-form-item :label="$lang('erate')" prop="erate">
+                <el-input v-model="formData.erate" style="width: 200px;"></el-input>
+            </el-form-item>
+            <el-form-item :label="$lang('irate')" prop="irate">
+                <el-input v-model="formData.irate" style="width: 200px;"></el-input>
+            </el-form-item>
+            <el-form-item :label="$lang('pvid')" prop="pvid">
+                <el-input v-model="formData.pvid" style="width: 200px;"></el-input>
+            </el-form-item>
+        </template>
+        <template v-if="type === 'stormctrl'">
+            <el-form-item :label="$lang('broadcast')" prop="broadcast" key="broadcast">
+                <el-input v-model="formData.broadcast" style="width: 200px;"></el-input>
+            </el-form-item>
+            <el-form-item :label="$lang('multicast')" prop="multicast" key="multicast">
+                <el-input v-model="formData.multicast" style="width: 200px;"></el-input>
+            </el-form-item>
+            <el-form-item :label="$lang('unicast')" prop="unicast" key="unicast">
+                <el-input v-model="formData.unicast" style="width: 200px;"></el-input>
+            </el-form-item>
+        </template>
+        <template v-if="type === 'mirror'">
+            <el-form-item :label="$lang('dst_port')" prop="dst_port">
+                <el-select v-model="formData.dst_port">
+                    <el-option label=" - " :value="0"></el-option>
+                    <template v-for="item in port">
+                        <el-option
+                            :label="getPortName(item.port_id)"
+                            :value="item.port_id"
+                            :disabled="item.port_id === data.src_port"
+                        ></el-option>
+                    </template>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('type')" prop="type">
+                <el-select v-model="formData.type">
+                    <template v-for="(item, index) in types">
+                        <el-option :label="item" :value="index"></el-option>
+                    </template>
+                </el-select>
+            </el-form-item>
+        </template>
+    </el-form>
+</template>
+
+<script>
+import { mapGetters, mapState } from "vuex";
+import { regRange } from "@/utils/validator";
+import { isFunction, isDef, toLower } from "@/utils/common";
+export default {
+    name: "portConfigForm",
+    computed: {
+        ...mapState(["port", "system"]),
+        ...mapGetters(["$lang", "validateMsg", "getPortName"])
+    },
+    props: {
+        type: {
+            type: String
+        },
+        data: {
+            type: Object
+        }
+    },
+    data() {
+        return {
+            types: [" - ", "ingress", "egress", "all"],
+            formData: {
+                admin_status: 1,
+                link_status: 1,
+                auto_neg: 1,
+                speed: "",
+                duplex: 1,
+                flow_ctrl: 1,
+                mtu: "",
+                media: "",
+                erate: "",
+                irate: "",
+                pvid: "",
+                broadcast: 0,
+                multicast: 0,
+                unicast: 0,
+                src_port: 0,
+                dst_port: 0,
+                type: 1
+            },
+            rules: {
+                mtu: [
+                    {
+                        validator: this.validateMtu,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                erate: [
+                    {
+                        validator: this.validateRate,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                irate: [
+                    {
+                        validator: this.validateRate,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                pvid: [
+                    {
+                        validator: this.validateVlan,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                broadcast: [
+                    {
+                        validator: this.validateCast,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                multicast: [
+                    {
+                        validator: this.validateCast,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                unicast: [
+                    {
+                        validator: this.validateCast,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                type: [
+                    {
+                        validator: this.validateType,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                dst_port: [
+                    {
+                        validator: this.validatePort,
+                        trigger: ["change", "blur"]
+                    }
+                ]
+            }
+        };
+    },
+    methods: {
+        validateMtu(rule, val, cb) {
+            if (this.type !== "sw_port_cfg") {
+                return cb();
+            }
+            if (!regRange(val, 128, 2000)) {
+                return cb(
+                    new Error(this.validateMsg("inputLength", 128, 2000))
+                );
+            }
+            cb();
+        },
+        validateRate(rule, val, cb) {
+            if (this.type !== "sw_port_cfg") {
+                return cb();
+            }
+            if (!regRange(val, 64, 1024000)) {
+                return cb(
+                    new Error(this.validateMsg("inputLength", 64, 1024000))
+                );
+            }
+            cb();
+        },
+        validateVlan(rule, val, cb) {
+            if (this.type !== "sw_port_cfg") {
+                return cb();
+            }
+            if (!regRange(val, 1, 4094)) {
+                return cb(new Error(this.validateMsg("inputRange", 1, 4094)));
+            }
+            cb();
+        },
+        validateCast(rule, val, cb) {
+            if (this.type !== "stormctrl") {
+                return cb();
+            }
+            if (!regRange(val, 0, 1488100)) {
+                return cb(
+                    new Error(this.validateMsg("inputLength", 0, 1488100))
+                );
+            }
+            cb();
+        },
+        validate(fn) {
+            if (isFunction(fn)) {
+                this.$refs["port-config-form"].validate(valid => {
+                    if (valid) {
+                        fn.call(this, this.formData);
+                    } else {
+                        fn.call(this, valid);
+                    }
+                });
+            } else {
+                return this.$refs["port-config-form"].validate();
+            }
+        },
+        validateType(rule, val, cb) {
+            if (val < 1 || val > 3) {
+                return cb(new Error(this.$lang("required")));
+            }
+            cb();
+        },
+        validatePort(rule, val, cb) {
+            if (val < 1 || val > this.port.length) {
+                return cb(new Error(this.$lang("required")));
+            }
+            cb();
+        },
+        init() {
+            Object.keys(this.formData).forEach(item => {
+                if (isDef(this.data[item])) {
+                    this.formData[item] = this.data[item];
+                }
+            });
+            if (isDef(this.data.speed)) {
+                if (
+                    toLower.call(this.data.speed) === "0M" ||
+                    this.data.speed === ""
+                ) {
+                    this.data.speed = "auto";
+                }
+            }
+        },
+        disabled(key, port_id) {
+            const { ponports, geports } = this.system;
+            const keys = ["admin_status", "duplex", "speed"];
+            if (keys.includes(key) && port_id <= ponports) {
+                return true;
+            }
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
