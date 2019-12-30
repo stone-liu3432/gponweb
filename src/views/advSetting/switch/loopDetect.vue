7f3c248810a4d2dfd893a20dea9f06dfc32ed9d5
stone-liu3432 <liushuang3432@163.com>
2019-12-30 17:20:16 +0800

    添加loopdetect页面

---
+++ src/views/advSetting/switch/loopDetect.vue
@@ -0,0 +1,125 @@
+<template>
+    <div>
+        <page-header type="none" :title="$lang('loop_detect')"></page-header>
+        <el-tabs v-model="activeName" type="card" style="margin-top: 20px;" @tab-click="tabClick">
+            <el-tab-pane :label="$lang('pon_loop_detect')" name="pon_loop_detect">
+                <loop-detect-detail
+                    :ld-status="ldStatus"
+                    :ld-info="ldInfo"
+                    type="pon_loop_detect"
+                    @refresh="getData"
+                ></loop-detect-detail>
+            </el-tab-pane>
+            <el-tab-pane :label="$lang('ge_loop_detect')" name="ge_loop_detect">
+                <loop-detect-detail
+                    :ld-status="ldStatus"
+                    :ld-info="ldInfo"
+                    type="ge_loop_detect"
+                    @refresh="getData"
+                ></loop-detect-detail>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import loopDetectDetail from "./loopDetect/detail";
+import { isFunction, isDef, isArray } from "@/utils/common";
+export default {
+    name: "loopDetect",
+    components: { loopDetectDetail },
+    computed: {
+        ...mapGetters(["$lang"])
+    },
+    data() {
+        return {
+            activeName: "pon_loop_detect",
+            ldInfo: [],
+            ldStatus: {}
+        };
+    },
+    created() {
+        this.getData();
+    },
+    methods: {
+        tabClick(tab) {
+            this.getData();
+        },
+        getData() {
+            const reqs = {
+                pon_loop_detect() {
+                    this.getPonStatus();
+                },
+                ge_loop_detect() {
+                    this.getGeStatus();
+                }
+            };
+            if (isFunction(reqs[this.activeName])) {
+                reqs[this.activeName].call(this);
+            }
+        },
+        getPonStatus() {
+            this.ldStatus = {};
+            this.$http
+                .get("/pon_loop_detect?form=loop_detect_config")
+                .then(res => {
+                    if (res.data.code === 1) {
+                        if (isDef(res.data.data)) {
+                            this.ldStatus = res.data.data;
+                            if (this.ldStatus.ld_status) {
+                                this.getPonInfo();
+                            }
+                        }
+                    }
+                })
+                .catch(err => {});
+        },
+        getPonInfo() {
+            this.ldInfo = [];
+            this.$http
+                .get("/pon_loop_detect?form=loop_detect_info")
+                .then(res => {
+                    if (res.data.code === 1) {
+                        if (isArray(res.data.data)) {
+                            this.ldInfo = res.data.data;
+                        }
+                    }
+                })
+                .catch(err => {});
+        },
+        getGeStatus() {
+            this.ldStatus = {};
+            this.$http
+                .get("/switch_loop_detect?form=loop_detect_config")
+                .then(res => {
+                    if (res.data.code === 1) {
+                        if (isDef(res.data.data)) {
+                            this.ldStatus = res.data.data;
+                            if (this.ldStatus.ld_status) {
+                                this.getGeInfo();
+                            }
+                        }
+                    }
+                })
+                .catch(err => {});
+        },
+        getGeInfo() {
+            this.ldInfo = [];
+            this.$http
+                .get("/switch_loop_detect?form=loop_detect_info")
+                .then(res => {
+                    if (res.data.code === 1) {
+                        if (isArray(res.data.data)) {
+                            this.ldInfo = res.data.data;
+                        }
+                    }
+                })
+                .catch(err => {});
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
---
+++ src/views/advSetting/switch/loopDetect/detail.vue
@@ -0,0 +1,393 @@
+<template>
+    <div>
+        <el-row class="loop-detect-status">
+            <el-col :span="6">{{ $lang('ld_status') }}</el-col>
+            <el-col :span="4">{{ status ? $lang('enable') : $lang('disable') }}</el-col>
+            <el-col :span="4">
+                <el-button type="primary" @click="setLdStatus">{{ $lang('config') }}</el-button>
+            </el-col>
+        </el-row>
+        <template v-if="status">
+            <el-row class="loop-detect-status">
+                <el-col :span="6">{{ $lang('detect_interval') }}</el-col>
+                <el-col :span="4">{{ ldStatus.detect_interval }}</el-col>
+                <el-col :span="4">
+                    <el-button type="primary" @click="setDetectInterval">{{ $lang('config') }}</el-button>
+                </el-col>
+            </el-row>
+            <el-row class="loop-detect-status">
+                <el-col :span="6">{{ $lang('recover_mode') }}</el-col>
+                <el-col :span="4">{{ ldStatus.recover_mode ? 'Manual' : 'Auto' }}</el-col>
+                <el-col :span="8">
+                    <el-button type="primary" @click="setLdRecoverMode">{{ $lang('config') }}</el-button>
+                    <el-button
+                        type="primary"
+                        v-if="isManual"
+                        @click="recoverManualDebounce"
+                    >{{ $lang('recover_manual') }}</el-button>
+                </el-col>
+            </el-row>
+            <el-row class="loop-detect-status">
+                <el-col :span="6">{{ $lang('recover_time') }}</el-col>
+                <el-col :span="4">{{ ldStatus.recover_time }}</el-col>
+                <el-col :span="4">
+                    <el-button type="primary" @click="setRecoverTime">{{ $lang('config') }}</el-button>
+                </el-col>
+            </el-row>
+            <h3>{{ $lang('ld_info') }}</h3>
+            <el-table
+                border
+                stripe
+                :data="ldInfo"
+                :cell-style="{ 'text-align': 'center' }"
+                :header-cell-style="{ 'text-align': 'center' }"
+            >
+                <template v-if="type === 'pon'">
+                    <el-table-column :label="`PON ID / ${$lang('onu_id')}`">
+                        <template
+                            slot-scope="scope"
+                        >{{ `${scope.row.port_id} / ${scope.row.onu_id}` }}</template>
+                    </el-table-column>
+                </template>
+                <template v-else>
+                    <el-table-column :label="$lang('port_id')">
+                        <template slot-scope="scope">{{ getPortName(scope.row.port_id) }}</template>
+                    </el-table-column>
+                </template>
+                <el-table-column :label="$lang('status')">
+                    <template slot-scope="scope">{{ scope.row.status ? 'Loop-Detect' : ' - ' }}</template>
+                </el-table-column>
+                <template v-if="type === 'pon'">
+                    <el-table-column :label="$lang('onu_status')">
+                        <template
+                            slot-scope="scope"
+                        >{{ scope.row.onu_status ? 'Add Black List' : ' - ' }}</template>
+                    </el-table-column>
+                    <el-table-column :label="`${$lang('loopback')} PON ID / ${$lang('onu_id')}`">
+                        <template
+                            slot-scope="scope"
+                        >{{ `${scope.row.ld_port_id} / ${scope.row.ld_onu_id}` }}</template>
+                    </el-table-column>
+                </template>
+                <template v-else>
+                    <el-table-column :label="$lang('port_status')">
+                        <template slot-scope="scope">{{ scope.row.port_status ? 'Blocked' : ' - ' }}</template>
+                    </el-table-column>
+                    <el-table-column :label="$lang('ld_port_id')">
+                        <template slot-scope="scope">{{ getPortName(scope.row.ld_port_id) }}</template>
+                    </el-table-column>
+                </template>
+            </el-table>
+        </template>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { isFunction, debounce } from "@/utils/common";
+import postData from "@/mixin/postData";
+export default {
+    name: "loopDetectDetail",
+    mixins: [postData],
+    computed: {
+        ...mapGetters(["$lang", "getPortName", "validateMsg"]),
+        status() {
+            return this.ldStatus.ld_status;
+        },
+        isManual() {
+            return this.ldStatus.recover_mode === 1;
+        }
+    },
+    data() {
+        return {
+            ld_status: 0,
+            recover_mode: 0
+        };
+    },
+    props: {
+        ldStatus: {
+            type: Object
+        },
+        ldInfo: {
+            type: Array
+        },
+        type: {
+            type: String
+        }
+    },
+    methods: {
+        setLdStatus() {
+            this.ld_status = this.status;
+            this.$msgbox({
+                title: this.$lang("config"),
+                message: (
+                    <div class="ld-form-content">
+                        <span>{this.$lang("ld_status")}</span>
+                        <select v-model={this.ld_status}>
+                            <option value={0}>{this.$lang("disable")}</option>
+                            <option value={1}>{this.$lang("enable")}</option>
+                        </select>
+                    </div>
+                ),
+                confirmButtonText: this.$lang("apply"),
+                cancelButtonText: this.$lang("cancel"),
+                showCancelButton: true
+            })
+                .then(_ => {
+                    if (this.ld_status === this.ldStatus.ld_status) {
+                        return;
+                    }
+                    this.submitActions("ld_status", this.ld_status);
+                })
+                .catch(_ => {});
+        },
+        setLdRecoverMode() {
+            this.recover_mode = this.ldStatus.recover_mode;
+            this.$msgbox({
+                title: this.$lang("config"),
+                message: (
+                    <div class="ld-form-content">
+                        <span>{this.$lang("recover_mode")}</span>
+                        <select v-model={this.recover_mode}>
+                            <option value={0}>Auto</option>
+                            <option value={1}>Manual</option>
+                        </select>
+                    </div>
+                ),
+                confirmButtonText: this.$lang("apply"),
+                cancelButtonText: this.$lang("cancel"),
+                showCancelButton: true
+            })
+                .then(_ => {
+                    if (this.recover_mode === this.ldStatus.recover_mode) {
+                        return;
+                    }
+                    this.submitActions("recover_mode", this.recover_mode);
+                })
+                .catch(_ => {});
+        },
+        setDetectInterval() {
+            this.$prompt(this.$lang("detect_interval"), {
+                title: this.$lang("config"),
+                confirmButtonText: this.$lang("apply"),
+                cancelButtonText: this.$lang("cancel"),
+                inputValue: this.ldStatus.detect_interval,
+                inputErrorMessage: this.validateMsg("inputRange", 3, 3600),
+                inputValidator: val => {
+                    const v = val >>> 0;
+                    return v >= 3 && v <= 3600 && !isNaN(v);
+                }
+            })
+                .then(({ value }) => {
+                    const val = value >>> 0;
+                    if (val === this.ldStatus.detect_interval) {
+                        return;
+                    }
+                    this.submitActions("detect_interval", val);
+                })
+                .catch(_ => {});
+        },
+        setRecoverTime() {
+            this.$prompt(this.$lang("recover_time"), {
+                title: this.$lang("config"),
+                confirmButtonText: this.$lang("apply"),
+                cancelButtonText: this.$lang("cancel"),
+                inputValue: this.ldStatus.recover_time,
+                inputErrorMessage: this.validateMsg("inputRange", 3, 3600),
+                inputValidator: val => {
+                    const v = val >>> 0;
+                    return v >= 3 && v <= 3600 && !isNaN(v);
+                }
+            })
+                .then(({ value }) => {
+                    const val = value >>> 0;
+                    if (val === this.ldStatus.recover_time) {
+                        return;
+                    }
+                    this.submitActions("recover_time", val);
+                })
+                .catch(_ => {});
+        },
+        submitActions(action, val) {
+            const ACTIONS = {
+                ld_status(val) {
+                    if (this.type === "pon_loop_detect") {
+                        return {
+                            url: "/pon_loop_detect?form=admin",
+                            data: {
+                                method: "set",
+                                param: {
+                                    ld_status: val
+                                }
+                            }
+                        };
+                    }
+                    if (this.type === "ge_loop_detect") {
+                        return {
+                            url: "/switch_loop_detect?form=admin",
+                            data: {
+                                method: "set",
+                                param: {
+                                    ld_status: val
+                                }
+                            }
+                        };
+                    }
+                },
+                recover_mode(val) {
+                    if (this.type === "pon_loop_detect") {
+                        return {
+                            url: "/pon_loop_detect?form=recover_mode",
+                            data: {
+                                method: "set",
+                                param: {
+                                    recover_mode: val
+                                }
+                            }
+                        };
+                    }
+                    if (this.type === "ge_loop_detect") {
+                        return {
+                            url: "/switch_loop_detect?form=recover_mode",
+                            data: {
+                                method: "set",
+                                param: {
+                                    recover_mode: val
+                                }
+                            }
+                        };
+                    }
+                },
+                detect_interval(val) {
+                    if (this.type === "pon_loop_detect") {
+                        return {
+                            url: "/pon_loop_detect?form=detect_interval",
+                            data: {
+                                method: "set",
+                                param: {
+                                    detect_interval: val
+                                }
+                            }
+                        };
+                    }
+                    if (this.type === "ge_loop_detect") {
+                        return {
+                            url: "/switch_loop_detect?form=detect_interval",
+                            data: {
+                                method: "set",
+                                param: {
+                                    detect_interval: val
+                                }
+                            }
+                        };
+                    }
+                },
+                recover_time(val) {
+                    if (this.type === "pon_loop_detect") {
+                        return {
+                            url: "/pon_loop_detect?form=recover_time",
+                            data: {
+                                method: "set",
+                                param: {
+                                    recover_time: val
+                                }
+                            }
+                        };
+                    }
+                    if (this.type === "ge_loop_detect") {
+                        return {
+                            url: "/switch_loop_detect?form=recover_time",
+                            data: {
+                                method: "set",
+                                param: {
+                                    recover_time: val
+                                }
+                            }
+                        };
+                    }
+                }
+            };
+            if (isFunction(ACTIONS[action])) {
+                const { url, data } = ACTIONS[action].call(this, val);
+                this.postData(url, data)
+                    .then(_ => {
+                        this.$emit("refresh");
+                    })
+                    .catch(_ => {});
+            }
+        },
+        recoverManual() {
+            let url = "/pon_loop_detect?form=recover_manual";
+            if (this.type === "ge_loop_detect") {
+                url = "/switch_loop_detect?form=recover_manual";
+            }
+            const post_params = {
+                method: "set",
+                param: {
+                    manual: 0
+                }
+            };
+            this.postData(url, post_params)
+                .then(_ => {
+                    this.$emit("refresh");
+                })
+                .catch(_ => {});
+        },
+        recoverManualDebounce() {
+            debounce(this.recoverManual, 1000, this);
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.loop-detect-status {
+    .base-font-style;
+    margin: 10px;
+}
+h3 {
+    color: @titleColor;
+}
+.ld-form-content {
+    span {
+        float: left;
+        width: 150px;
+        line-height: 30px;
+        & + div {
+            float: left;
+        }
+    }
+    select {
+        .base-font-style;
+        width: 200px;
+        height: 30px;
+        text-indent: 10px;
+        border-radius: 3px;
+    }
+    input[type="text"] {
+        .base-font-style;
+        position: relative;
+        width: 200px;
+        height: 30px;
+        border-radius: 3px;
+        text-indent: 10px;
+        border: @border-style;
+        outline: none;
+        &:focus {
+            border: 1px solid @titleColor;
+        }
+    }
+    &:after {
+        content: "";
+        display: table;
+        clear: both;
+    }
+    p {
+        line-height: 18px;
+        color: red;
+        font-size: 12px;
+        text-indent: 10px;
+        opacity: 0.7;
+    }
+}
+</style>
