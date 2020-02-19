31058e1c0cff137ae80629f7c0d606afdc98f5f9
stone-liu3432 <liushuang3432@163.com>
2020-02-19 17:07:27 +0800

    添加分页，PON和GE分页来展示

--- src/views/page/status.vue
+++ src/views/page/status.vue
@@ -1,28 +1,32 @@
 <template>
     <div style="margin: 20px;">
-        <h3>PON</h3>
-        <el-row :gutter="40">
-            <template v-for="item in pon">
-                <el-col :span="6" style="margin: 20px 0;">
-                    <port-card type="pon" :data="item"></port-card>
-                </el-col>
-            </template>
-        </el-row>
-        <h3>GE</h3>
-        <el-row :gutter="40">
-            <template v-for="item in port">
-                <template v-if="item.port_id > system.ponports">
-                    <el-col :span="6" style="margin: 20px 0;">
-                        <template v-if="item.media === 'fiber'">
-                            <port-card type="fiber" :data="item"></port-card>
+        <el-tabs v-model="activeName" type="card">
+            <el-tab-pane label="PON" name="pon">
+                <el-row :gutter="40">
+                    <template v-for="item in pon">
+                        <el-col :span="6" style="margin: 20px 0;">
+                            <port-card type="pon" :data="item"></port-card>
+                        </el-col>
+                    </template>
+                </el-row>
+            </el-tab-pane>
+            <el-tab-pane :label="$lang('uplink')" name="uplink">
+                <el-row :gutter="40">
+                    <template v-for="item in port">
+                        <template v-if="item.port_id > system.ponports">
+                            <el-col :span="6" style="margin: 20px 0;">
+                                <template v-if="item.media === 'fiber'">
+                                    <port-card type="fiber" :data="item"></port-card>
+                                </template>
+                                <template v-else>
+                                    <port-card type="rj45" :data="item"></port-card>
+                                </template>
+                            </el-col>
                         </template>
-                        <template v-else>
-                            <port-card type="rj45" :data="item"></port-card>
-                        </template>
-                    </el-col>
-                </template>
-            </template>
-        </el-row>
+                    </template>
+                </el-row>
+            </el-tab-pane>
+        </el-tabs>
     </div>
 </template>
 
@@ -33,12 +37,14 @@ export default {
     name: "status",
     components: { portCard },
     data() {
-        return {};
+        return {
+            activeName: "pon"
+        };
     },
     inject: ["updateNavScrollbar"],
     computed: {
         ...mapState(["system", "pon", "port"]),
-        ...mapGetters(["getPortName"])
+        ...mapGetters(["getPortName", "$lang"])
     },
     updated() {
         this.$nextTick(_ => {
@@ -48,8 +54,8 @@ export default {
     mounted() {
         // 定时刷新数据
         const interval = setInterval(_ => {
-            this.getPon();
-            this.getPort();
+            this.activeName === "pon" && this.getPon();
+            this.activeName === "uplink" && this.getPort();
         }, 5000);
         this.$once("hook:beforeDestroy", _ => {
             clearInterval(interval);
