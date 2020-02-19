ff8d12e513929b57e1b95c9ef4822a854b02e924
stone-liu3432 <liushuang3432@163.com>
2020-02-19 17:04:05 +0800

    添加模板类型

--- src/utils/commonData.js
+++ src/utils/commonData.js
@@ -6,3 +6,12 @@ export const LINK_STATUS = ["link_down", "link_up"];
 export const STATUS = ["offline", "online"];
 
 export const ADVANCED_MENU = "advanced_setting";
+
+// DBA模板带宽类型
+export const DBA_PROFILE_TYPES = {
+    1: ["fix"],
+    2: ["assure"],
+    3: ["assure", "max"],
+    4: ["max"],
+    5: ["fix", "assure", "max"]
+};
