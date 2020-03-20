a3ad8c1c681cc22c95fea7d9e36886dfcfd2a28a
stone-liu3432 <liushuang3432@163.com>
2020-03-20 14:48:56 +0800

    添加onu管理页

--- src/views/page/onuAllow.vue
+++ src/views/page/onuAllow.vue
@@ -1,34 +1,199 @@
 <template>
     <div style="margin: 20px;">
-        <page-header @port-change="portChange" type="none">
+        <page-header @port-change="portChange" type="pon">
             <div slot="title">{{ $lang('onu_allow') }}</div>
         </page-header>
+        <el-table
+            :data="showTable"
+            border
+            stripe
+            @expand-change="updateScrollbar"
+            @row-click="rowClick"
+            ref="ont-info-table"
+        >
+            <el-table-column type="expand">
+                <template slot-scope="props">
+                    <el-row style="margin: 12px 0;">
+                        <el-col :span="8">
+                            <span class="collspace-title">sn:</span>
+                            <span>{{ props.row.sn }}</span>
+                        </el-col>
+                        <el-col :span="8">
+                            <span class="collspace-title">loid:</span>
+                            <span>{{ props.row.loid }}</span>
+                        </el-col>
+                        <el-col :span="8">
+                            <span class="collspace-title">loidpasswd:</span>
+                            <span>{{ props.row.loidpasswd }}</span>
+                        </el-col>
+                    </el-row>
+                    <el-row style="margin: 12px 0;">
+                        <el-col :span="8">
+                            <span class="collspace-title">ont_description:</span>
+                            <span>{{ props.row.ont_description }}</span>
+                        </el-col>
+                        <el-col :span="8">
+                            <span class="collspace-title">last_d_cause:</span>
+                            <span>{{ props.row.last_d_cause }}</span>
+                        </el-col>
+                        <el-col :span="8">
+                            <span class="collspace-title">last_dg_time:</span>
+                            <span>{{ props.row.last_dg_time }}</span>
+                        </el-col>
+                    </el-row>
+                    <el-row style="margin: 12px 0;">
+                        <el-col :span="8">
+                            <span class="collspace-title">uptime:</span>
+                            <span>{{ props.row.uptime }}</span>
+                        </el-col>
+                        <el-col :span="8">
+                            <span class="collspace-title">lineprof_id:</span>
+                            <span>{{ props.row.lineprof_id }}</span>
+                        </el-col>
+                        <el-col :span="8">
+                            <span class="collspace-title">lineprof_name:</span>
+                            <span>{{ props.row.lineprof_name }}</span>
+                        </el-col>
+                    </el-row>
+                    <el-row style="margin: 12px 0;">
+                        <el-col :span="8">
+                            <span class="collspace-title">srvprof_id:</span>
+                            <span>{{ props.row.srvprof_id }}</span>
+                        </el-col>
+                        <el-col :span="8">
+                            <span class="collspace-title">srvprof_name:</span>
+                            <span>{{ props.row.srvprof_name }}</span>
+                        </el-col>
+                    </el-row>
+                </template>
+            </el-table-column>
+            <el-table-column label="ont_id">
+                <template
+                    slot-scope="scope"
+                >{{ `${getPortName((scope.row.identifier >> 8) & 0xff)}/${(scope.row.identifier) & 0xff}` }}</template>
+            </el-table-column>
+            <el-table-column label="ont_name" prop="ont_name"></el-table-column>
+            <el-table-column label="state" prop="state"></el-table-column>
+            <el-table-column label="rstate" prop="rstate"></el-table-column>
+            <el-table-column label="cstate" prop="cstate"></el-table-column>
+            <el-table-column label="mstate" prop="mstate"></el-table-column>
+            <el-table-column label="authmode" prop="authmode"></el-table-column>
+            <el-table-column label="last_u_time" prop="last_u_time"></el-table-column>
+            <el-table-column label="last_d_time" prop="last_d_time"></el-table-column>
+            <el-table-column :label="$lang('config')" width="100" prop="config">
+                <template slot-scope="scope">
+                    <el-button type="text" @click="openDialog(scope.row)">{{ $lang('config') }}</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <el-pagination
+            style="float: right; margin: 12px 0;"
+            :current-page.sync="currentPage"
+            :page-sizes="[10, 20, 30, 50]"
+            :page-size.sync="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="ontList.length"
+            hide-on-single-page
+        ></el-pagination>
+        <el-dialog :title="$lang('config')" :visible.sync="dialogVisible" width="500px">
+            <onu-allow-form ref="onu-allow-form"></onu-allow-form>
+            <span slot="footer">
+                <el-button @click="dialogVisible = false">{{ $lang('cancel') }}</el-button>
+                <el-button type="primary" @click="submitForm('onu-allow-form')">{{ $lang('apply') }}</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import { isFunction, isArray } from "@/utils/common";
+import onuAllowForm from "./onuAllow/onuAllowForm";
+import postData from "@/mixin/postData";
 export default {
     name: "onuAllow",
+    components: { onuAllowForm },
+    mixins: [postData],
     computed: {
-        ...mapGetters(["$lang"])
+        ...mapGetters(["$lang", "getPortName"]),
+        showTable() {
+            this.ontList = this.allOntList.filter(
+                item => ((item.identifier >> 8) & 0xff) === this.port_id
+            );
+            const start = (this.currentPage - 1) * this.pageSize;
+            return this.ontList.slice(start, start + this.pageSize);
+        }
     },
     data() {
         return {
-            port_id: 0
+            port_id: 0,
+            allOntList: [],
+            ontList: [],
+            currentPage: 1,
+            pageSize: 10,
+            dialogVisible: false
         };
     },
-    created() {},
+    created() {
+        this.getData();
+    },
     inject: ["updateNavScrollbar"],
     updated() {
-        this.$nextTick(_ => {
-            this.updateNavScrollbar();
-        });
+        this.updateScrollbar();
     },
     methods: {
         portChange(port_id) {
             this.port_id = port_id;
+        },
+        getData() {
+            this.allOntList = [];
+            this.currentPage = 1;
+            this.$http
+                .get("/gponont_mgmt?form=base")
+                .then(res => {
+                    if (res.data.code === 1) {
+                        if (isArray(res.data.data)) {
+                            this.allOntList = res.data.data;
+                        }
+                    }
+                })
+                .catch(err => {});
+        },
+        updateScrollbar() {
+            this.$nextTick(_ => {
+                this.updateNavScrollbar();
+            });
+        },
+        rowClick(row, col, e) {
+            col.property !== "config" &&
+                this.$refs["ont-info-table"].toggleRowExpansion(row);
+        },
+        openDialog(row) {
+            this.dialogVisible = true;
+            this.$nextTick(_ => {
+                this.$refs["onu-allow-form"].init(row);
+            });
+        },
+        submitForm(formName) {
+            this.$refs[formName].validate(formData => {
+                if (formData) {
+                    const url = "/gponont_mgmt?form=info",
+                        data = {
+                            method: "set",
+                            param: {
+                                identifier: formData.identifier,
+                                ont_name: formData.ont_name,
+                                ont_description: formData.ont_description
+                            }
+                        };
+                    this.postData(url, data)
+                        .then(_ => {
+                            this.getData();
+                        })
+                        .catch(_ => {});
+                    this.dialogVisible = false;
+                }
+            });
         }
     },
     watch: {}
@@ -36,4 +201,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.collspace-title {
+    margin: 0 12px 0 0;
+    display: inline-block;
+    vertical-align: middle;
+    max-width: 25%;
+    & + span {
+        display: inline-block;
+        vertical-align: middle;
+        max-width: 75%;
+        word-wrap: break-word;
+        word-break: break-all;
+        color: @titleColor;
+    }
+}
 </style>
---
+++ src/views/page/onuAllow/onuAllowForm.vue
@@ -0,0 +1,100 @@
+<template>
+    <el-form :model="form" label-width="140px" :rules="rules" ref="onu-allow-form">
+        <el-form-item label="ont_id" prop="identifier">
+            <span>{{ ont_id }}</span>
+        </el-form-item>
+        <el-form-item label="ont_name" prop="ont_name">
+            <el-input v-model="form.ont_name"></el-input>
+        </el-form-item>
+        <el-form-item label="ont_description" prop="ont_description">
+            <el-input v-model="form.ont_description"></el-input>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { regLength } from "@/utils/validator";
+import { isFunction } from "@/utils/common";
+export default {
+    name: "onuAllowForm",
+    computed: {
+        ...mapGetters(["$lang", "getPortName", "validateMsg"]),
+        ont_id() {
+            const bytes = this.form.identifier;
+            return `${this.getPortName((bytes >> 8) & 0xff)}/${bytes & 0xff}`;
+        }
+    },
+    data() {
+        return {
+            cache: {},
+            form: {
+                identifier: 0,
+                ont_name: "",
+                ont_description: ""
+            },
+            rules: {
+                ont_name: [
+                    {
+                        validator: this.validateName,
+                        trigger: ["change", "blur"]
+                    }
+                ],
+                ont_description: [
+                    {
+                        validator: this.validateDesc,
+                        trigger: ["change", "blur"]
+                    }
+                ]
+            }
+        };
+    },
+    methods: {
+        init(data) {
+            this.$refs["onu-allow-form"].resetFields();
+            Object.keys(this.form).forEach(key => {
+                this.form[key] = data[key];
+                // 校验用，无修改时提示
+                this.cache[key] = data[key];
+            });
+        },
+        validateName(rule, val, cb) {
+            if (!regLength(val, 1, 32)) {
+                return cb(new Error(this.validateMsg("inputLength", 1, 32)));
+            }
+            cb();
+        },
+        validateDesc(rule, val, cb) {
+            if (!regLength(val, 1, 128)) {
+                return cb(new Error(this.validateMsg("inputLength", 1, 128)));
+            }
+            cb();
+        },
+        validate(fn) {
+            this.$refs["onu-allow-form"].validate(valid => {
+                if (isFunction(fn)) {
+                    if (valid) {
+                        let flag = true;
+                        Object.keys(this.form).forEach(key => {
+                            if (this.cache[key] !== this.form[key]) {
+                                flag = false;
+                            }
+                        });
+                        if (flag) {
+                            return this.$message.error(
+                                this.$lang("modify_tips")
+                            );
+                        }
+                        fn.call(this, this.form);
+                    } else {
+                        fn.call(this, valid);
+                    }
+                }
+            });
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+</style>
