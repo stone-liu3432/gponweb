c54baa1d465ae55261f86ac34e02e1a143d7310e
stone-liu3432 <liushuang3432@163.com>
2020-03-20 14:49:18 +0800

    添加onu列表的mock

--- src/mock/page/onuAllow.js
+++ src/mock/page/onuAllow.js
@@ -8,15 +8,15 @@ Mock.mock("/gponont_mgmt?form=base", "get", _ => {
     const data = Array.from({ length: n }).map(_ => ({
         identifier: (Random.range(1, 16) << 8) | Random.range(0, 127),
         ont_name: Random.word(1, 32),
-        state: Random.word(1, 32),
-        rstate: Random.word(1, 32),
-        cstate: Random.word(1, 32),
-        mstate: Random.word(1, 32),
+        state: Random.word(1, 8),
+        rstate: Random.word(1, 8),
+        cstate: Random.word(1, 8),
+        mstate: Random.word(1, 8),
         authmode: Random.range(0, 1),
         sn: Random.word(1, 32),
         loid: Random.word(1, 32),
         loidpasswd: Random.word(1, 32),
-        description: Random.word(1, 32),
+        ont_description: Random.word(1, 32),
         last_d_cause: Random.datetime(),
         last_d_time: Random.datetime(),
         last_u_time: Random.datetime(),
@@ -27,9 +27,12 @@ Mock.mock("/gponont_mgmt?form=base", "get", _ => {
         srvprof_id: Random.range(1, 2048),
         srvprof_name: Random.word(1, 32)
     }));
+    data.sort((a, b) => (a.identifier & 0xff) - (b.identifier & 0xff));
     return {
         code: 1,
         message: "success",
         data
     };
 });
+
+Mock.mock("/gponont_mgmt?form=info", "post", "@BASESUCCESS");
