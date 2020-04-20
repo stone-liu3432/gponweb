4f2877c257048c1abdb5fa39628028d3500b195e
stone-liu3432 <liushuang3432@163.com>
2020-04-20 15:36:43 +0800

    添加删除ont的mixin

---
+++ src/mixin/onu/rebootOnt.js
@@ -0,0 +1,35 @@
+"use strict";
+export default {
+    methods: {
+        rebootOnt(identifier) {
+            return this.$confirm(this.$lang("confirm_reboot_onu") + " ?")
+                .then((_) => {
+                    const post_params = {
+                        method: "set",
+                        param: {
+                            identifier,
+                            flags: 0x4,
+                            ont_name: "",
+                            ont_description: "",
+                        },
+                    };
+                    return this.$http
+                        .post("/gponont_mgmt?form=info", post_params)
+                        .then((res) => {
+                            if (res.data.code === 1) {
+                                this.$message.success(
+                                    this.$lang("reboot_onu", "st_success")
+                                );
+                            } else {
+                                this.$message.error(
+                                    `(${res.data.code}) ${res.data.message}`
+                                );
+                                return Promise.reject();
+                            }
+                        })
+                        .catch((err) => {});
+                })
+                .catch((_) => {});
+        },
+    },
+};
