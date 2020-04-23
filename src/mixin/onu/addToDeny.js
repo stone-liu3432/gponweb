a4cfede545e4353faa846a710e37060573f3f8cb
stone-liu3432 <liushuang3432@163.com>
2020-04-23 16:05:17 +0800

    修改addToDeny的返回实现

--- src/mixin/onu/addToDeny.js
+++ src/mixin/onu/addToDeny.js
@@ -2,28 +2,23 @@
 export default {
     methods: {
         addToDeny(url, data) {
-            return new Promise((resolve, reject) => {
-                this.$confirm(this.$lang("tips_add_deny_onu"))
-                    .then(_ => {
-                        this.$http
-                            .post(url, data)
-                            .then(res => {
-                                if (res.data.code === 1) {
-                                    this.$message.success(
-                                        this.$lang("setting_ok")
-                                    );
-                                    resolve();
-                                } else {
-                                    this.$message.error(
-                                        `(${res.data.code}) ${res.data.message}`
-                                    );
-                                    reject(res.data);
-                                }
-                            })
-                            .catch(err => {});
-                    })
-                    .catch(_ => {});
-            });
-        }
-    }
+            return this.$confirm(this.$lang("tips_add_deny_onu"))
+                .then(() => {
+                    return this.$http
+                        .post(url, data)
+                        .then((res) => {
+                            if (res.data.code === 1) {
+                                this.$message.success(this.$lang("setting_ok"));
+                            } else {
+                                this.$message.error(
+                                    `(${res.data.code}) ${res.data.message}`
+                                );
+                                return Promise.reject(res.data);
+                            }
+                        })
+                        .catch((err) => {});
+                })
+                .catch(() => {});
+        },
+    },
 };
