aadc357eb62232cea266bedbfd393f1d679581fe
stone-liu3432 <liushuang3432@163.com>
2020-04-23 16:05:49 +0800

    修改rebootOnu的返回实现

--- src/mixin/onu/rebootOnt.js
+++ src/mixin/onu/rebootOnt.js
@@ -24,7 +24,7 @@ export default {
                                 this.$message.error(
                                     `(${res.data.code}) ${res.data.message}`
                                 );
-                                return Promise.reject();
+                                return Promise.reject(res.data);
                             }
                         })
                         .catch((err) => {});
