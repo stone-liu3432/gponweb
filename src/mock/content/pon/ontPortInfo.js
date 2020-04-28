894c2c26f5a72d0dbfc1ecae1ae9fbb94b066bd9
stone-liu3432 <liushuang3432@163.com>
2020-04-28 10:25:55 +0800

    添加ont info的mock

---
+++ src/mock/content/pon/ontPortInfo.js
@@ -0,0 +1,66 @@
+import Mock from "@/mock";
+
+const { Random } = Mock;
+
+Mock.mock(
+    /\/gponont_mgmt\?form=ethport&port_id=\d+&ont_id=\d+/,
+    "get",
+    ({ url }) => {
+        const identifier =
+            (url.match(/(?<=port_id=)\d+/) << 8) |
+            (url.match(/(?<=ont_id=)\d+/) >>> 0);
+        const n = Random.range(1, 4);
+        const data = Array.from({ length: n }).map((item, index) => {
+            return {
+                identifier,
+                ethport_id: index + 1,
+                porttype: "GE",
+                epspeed: Random.pick([0, 10, 100, 1000]),
+                epduplex: Random.range(0, 1),
+                epstatus: Random.range(0, 1),
+                ring: Random.range(1, 2),
+            };
+        });
+        return {
+            code: 1,
+            message: "success",
+            data,
+        };
+    }
+);
+
+Mock.mock("/onumgmt?form=natvie_vlan", "post", "@BASESUCCESS");
+
+Mock.mock("/onumgmt?form=port_vlan", "post", "@BASESUCCESS");
+
+Mock.mock("/onumgmt?form=port_attr", "post", "@BASESUCCESS");
+
+Mock.mock(
+    /\/gponont_mgmt\?form=port_attr&port_id=\d+&ont_id=\d+/,
+    "get",
+    ({ url }) => {
+        const identifier =
+            url.match(/(?<=port_id=)\d+/ << 8) |
+            (url.match(/(?<=ont_id=)\d+/) >>> 0);
+        const data = Array.from({ length: Random.range(1, 8) }).map(
+            (item, index) => ({
+                identifier,
+                uniport: index + 1,
+                unitype: Random.range(0, 1),
+                epspeed: Random.pick([10, 100, 1000, 0xffff]),
+                epduplex: Random.range(0, 1),
+                epautoneg: Random.range(0, 1),
+                flow_ctrl: Random.range(0, 1),
+                adminstate: Random.range(0, 1),
+                na_vlan_id: Random.range(1, 4094),
+                na_vlan_pri: Random.range(0, 8),
+                ring: Random.range(1, 2),
+            })
+        );
+        return {
+            code: 1,
+            message: "success",
+            data,
+        };
+    }
+);
