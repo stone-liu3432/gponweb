54d44655707d1c6b1e8bf55bbc4c82c8a55628b1
stone-liu3432 <liushuang3432@163.com>
2020-04-30 10:19:12 +0800

    添加pon设置的mock

---
+++ src/mock/content/pon/ponSetting.js
@@ -0,0 +1,31 @@
+import Mock from "@/mock";
+
+const { Random } = Mock;
+
+Mock.mock("/gponmgmt?form=gpon_setting", "get", () => {
+    const gpon_setting = Array.from({ length: 16 }).map((_item, index) => ({
+        port_id: index + 1,
+        autofind: Random.range(0, 1),
+        laser: Random.range(0, 1),
+        auth_type: Random.range(0, 1),
+        lineprof_id: Random.range(1, 2048),
+        srvprof_id: Random.range(1, 2048),
+    }));
+    return {
+        code: 1,
+        message: "success",
+        data: {
+            af_aging_time: Random.pick([0, Random.range(100, 300)]),
+            af_interval: Random.range(2, 10),
+            gpon_setting,
+        },
+    };
+});
+
+Mock.mock(/\/gponmgmt\?form=laser&port_id=\d+/, "post", "@BASESUCCESS");
+
+Mock.mock(/\/gponmgmt\?form=ont_autofind&port_id=\d+/, "post", "@BASESUCCESS");
+
+Mock.mock(/\/gponmgmt\?form=ont_auth&port_id=\d+/, "post", "@BASESUCCESS");
+
+Mock.mock(/\/gponmgmt\?form=af_param/, "post", "@BASESUCCESS");
