957db0d77225ae4d36558cfe0059a99278c5a90f
stone-liu3432 <liushuang3432@163.com>
2020-04-30 10:18:43 +0800

    添加ont端口vlan的mock

---
+++ src/mock/content/pon/opVlan.js
@@ -0,0 +1,34 @@
+import Mock from "@/mock";
+
+const { Random } = Mock;
+
+Mock.mock(
+    /\/gponont_mgmt\?form=port_vlan&port_id=\d+&ont_id=\d+/,
+    "get",
+    ({ url }) => {
+        const identifier =
+            (url.match(/(?<=port_id=)\d+/) << 8) |
+            (url.match(/(?<=ont_id=)\d+/) >>> 0);
+        const portvlan = Array.from({ length: Random.range(1, 8) }).map(
+            (item, index) => {
+                return {
+                    uniport: index + 1,
+                    unitype: Random.range(0, 1),
+                    mode: Random.range(1, 4),
+                    svlanid: Random.range(1, 4094),
+                    svlanpri: Random.range(0, 8),
+                    cvlanid: Random.range(1, 4094),
+                    cvlanpri: Random.range(0, 8),
+                };
+            }
+        );
+        return {
+            code: 1,
+            message: "success",
+            data: {
+                identifier,
+                portvlan,
+            },
+        };
+    }
+);
