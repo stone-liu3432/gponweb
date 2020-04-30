4dd4e7a74b4aad5c6c0b32ad2225dbccc8b9d693
stone-liu3432 <liushuang3432@163.com>
2020-04-30 10:20:23 +0800

    添加风扇转速的mock

--- src/mock/content/system/runningStatus.js
+++ src/mock/content/system/runningStatus.js
@@ -1,12 +1,13 @@
 import Mock from "@/mock";
+import { Random } from "mockjs";
 
 Mock.mock("/board?info=cpu", "get", {
     code: 1,
     message: "success",
     data: {
         cpu_usage: "@RANGE(0,100)",
-        memory_usage: "@RANGE(0,100)"
-    }
+        memory_usage: "@RANGE(0,100)",
+    },
 });
 
 Mock.mock("/time?form=info", "get", {
@@ -19,11 +20,11 @@ Mock.mock("/time?form=info", "get", {
             "@RANGE(1,28)",
             "@RANGE(0,23)",
             "@RANGE(0,59)",
-            "@RANGE(0,59)"
+            "@RANGE(0,59)",
         ],
         uptime: ["@RANGE(1,9)", "@RANGE(0,23)", "@RANGE(0,59)", "@RANGE(0,59)"],
-        timezone: "@RANGE(1,99)"
-    }
+        timezone: "@RANGE(1,99)",
+    },
 });
 
 Mock.mock("/board?info=setting_board", "get", {
@@ -33,6 +34,18 @@ Mock.mock("/board?info=setting_board", "get", {
         name: "中国深圳市鸿升光通讯设备有限公司",
         addr: "深圳市坂田街道杨美村德众工业园A栋6楼",
         email: "hsgq@hsgq.com",
-        phone: "0755—81755556"
-    }
+        phone: "0755—81755556",
+    },
+});
+
+Mock.mock("/board?info=fan", "get", () => {
+    const data = Array.from({ length: 3 }).map((item, index) => ({
+        fanid: index + 1,
+        speed: Random.range(0, 255),
+    }));
+    return {
+        code: 1,
+        message: "success",
+        data,
+    };
 });
