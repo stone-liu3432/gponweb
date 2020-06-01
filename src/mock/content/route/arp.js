02ab9ec9474f652734395cb3b0d3b81ba7baae6c
stone-liu3432 <liushuang3432@163.com>
2020-06-01 09:48:55 +0800

    添加路由arp管理的mock

---
+++ src/mock/content/route/arp.js
@@ -0,0 +1,21 @@
+import Mock from "@/mock";
+
+const { Random } = Mock;
+
+Mock.mock("/switch_route?form=arp_table", "get", () => {
+    const data = Array.from({ length: Random.range(0, 4094) }).map(
+        (item, index) => ({
+            ipaddress: Random.ip(),
+            macaddress: Random.mac(),
+            portid: Random.range(17, 24),
+            vlanid: index + 1,
+        })
+    );
+    return {
+        code: 1,
+        message: "success",
+        data,
+    };
+});
+
+Mock.post("/switch_route?form=arp_flush");
