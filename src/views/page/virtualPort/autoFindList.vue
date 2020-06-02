c469b7e98f27ef1e9d81d7145be34b581e13a8f6
stone-liu3432 <liushuang3432@163.com>
2020-06-02 13:36:30 +0800

    添加svp_af_id

--- src/views/page/virtualPort/autoFindList.vue
+++ src/views/page/virtualPort/autoFindList.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
+        <div style="margin: 12px;">
+            <el-button type="primary" size="mini" @click="refreshData">{{ $lang('refresh') }}</el-button>
+        </div>
         <el-table :data="showList" border>
+            <el-table-column :label="$lang('svp_af_id')" prop="svp_af_id"></el-table-column>
             <el-table-column :label="$lang('port_id')">
                 <template slot-scope="scope">{{ getPortName(scope.row.port_id) }}</template>
             </el-table-column>
             <el-table-column :label="$lang('ont_id')" prop="ont_id"></el-table-column>
             <el-table-column :label="$lang('gemport')" prop="gemport"></el-table-column>
-            <el-table-column :label="$lang('user_vlan')" prop="user_vlan"></el-table-column>
+            <el-table-column :label="$lang('user_vlan')" prop="user_vlan">
+                <template slot-scope="scope">{{ scope.row.user_vlan || '-' }}</template>
+            </el-table-column>
             <el-table-column :label="$lang('config')">
                 <template slot-scope="scope">
                     <el-button
@@ -26,13 +32,13 @@
             hide-on-single-page
         ></el-pagination>
         <el-dialog :visible.sync="dialogVisible" width="640px">
-            <span slot="title"></span>
-            <virtual-port-form type="add" :data="dialogData" ref="virtual-port-form"></virtual-port-form>
+            <span slot="title">{{ $lang('add') }}</span>
+            <add-vp-from-af-form ref="add-vp-from-af-form"></add-vp-from-af-form>
             <div slot="footer">
                 <el-button @click="dialogVisible = false;">{{ $lang('cancel') }}</el-button>
                 <el-button
                     type="primary"
-                    @click="submitForm('virtual-port-form')"
+                    @click="submitForm('add-vp-from-af-form')"
                 >{{ $lang('apply') }}</el-button>
             </div>
         </el-dialog>
@@ -41,12 +47,12 @@
 
 <script>
 import { mapGetters } from "vuex";
-import { isArray } from "@/utils/common";
-import virtualPortForm from "./virtualPortForm";
+import { isArray, debounce } from "@/utils/common";
+import addVpFromAfForm from "./addVpFromAutofind";
 import postData from "@/mixin/postData";
 export default {
     name: "autoFindList",
-    components: { virtualPortForm },
+    components: { addVpFromAfForm },
     mixins: [postData],
     computed: {
         ...mapGetters(["$lang", "getPortName"]),
@@ -66,8 +72,7 @@ export default {
             findList: [],
             currentPage: 1,
             pageSize: 10,
-            dialogVisible: false,
-            dialogData: {}
+            dialogVisible: false
         };
     },
     created() {
@@ -88,31 +93,22 @@ export default {
                 .catch(err => {});
         },
         openDialog(row) {
-            this.dialogData = row;
             this.dialogVisible = true;
             this.$nextTick(_ => {
-                this.$refs["virtual-port-form"].init(form => {
-                    Object.keys(form).forEach(key => {
-                        key in row && (form[key] = row[key]);
-                    });
-                });
+                this.$refs["add-vp-from-af-form"].init(row);
             });
         },
         submitForm(formName) {
             this.$refs[formName].validate(formData => {
                 if (formData) {
-                    const url = "/switch_svp?form=svp_create",
+                    const url = "/switch_svp?form=svp_af_create",
                         data = {
                             method: "add",
                             param: {
-                                svp_id: formData.svp_id,
-                                svlan: formData.svlan,
-                                port_id: formData.port_id,
-                                ont_id: formData.ont_id,
-                                gemport: formData.gemport,
-                                user_vlan: formData.user_vlan,
+                                svp_af_id: formData.svp_af_id,
+                                new_svlan: formData.new_svlan,
                                 tag_action: formData.tag_action,
-                                inner_vlan: formData.inner_vlan
+                                inner_vlan: formData.inner_vlan || 0
                             }
                         };
                     this.postData(url, data)
@@ -123,6 +119,9 @@ export default {
                         .catch(_ => {});
                 }
             });
+        },
+        refreshData() {
+            debounce(this.getData, 1000, this);
         }
     }
 };
