b9861f1f42b61691d101fb7c801a1d4ce95bcb17
stone-liu3432 <liushuang3432@163.com>
2020-06-03 10:19:46 +0800

    svlan替换为new_svlan

--- src/mock/content/virtualPort/virtualPort.js
+++ src/mock/content/virtualPort/virtualPort.js
@@ -8,24 +8,25 @@ const Random = Mock.Random;
 
 Mock.mock("/switch_svp?form=svp_table", "get", () => {
     const n = Random.range(0, 8191);
-    const data = Array.from({ length: n }).map(item => ({
-        svp_id: Random.range(0, 8191),
-        svlan: Random.range(1, 4094),
+    const data = Array.from({ length: n }).map((item, index) => ({
+        svp_id: index + 1,
+        new_svlan: Random.range(1, 4094),
         port_id: Random.range(1, 16),
         ont_id: Random.range(0, 127),
         gemport: Random.range(1, 32),
+        svp_type: Random.range(1, 2),
         user_vlan: Random.range(1, 4094),
         tag_action: Random.range(1, 5),
         inner_vlan: Random.range(1, 4094),
         state: Random.range(0, 1),
         admin_status: Random.range(0, 1),
-        desc: Random.word(0, 64)
+        desc: Random.word(0, 64),
+        install_mode: Random.range(1, 2),
     }));
-    data.sort((a, b) => a.svp_id - b.svp_id);
     return {
         code: 1,
         message: "success",
-        data
+        data,
     };
 });
 
@@ -43,16 +44,16 @@ Mock.mock("/switch_svp?form=svp_admin_status", "post", "@BASESUCCESS");
 Mock.mock("/switch_svp?form=svp_auto_profile", "get", () => {
     const n = Random.range(0, 16);
     let i = 1;
-    const data = Array.from({ length: n }).map(item => ({
-        svlan: Random.range(1, 4094),
+    const data = Array.from({ length: n }).map((item, index) => ({
+        new_svlan: Random.range(1, 4094),
         port_id: i++,
         tag_action: Random.range(1, 5),
-        inner_vlan: Random.range(0, 4094)
+        inner_vlan: Random.range(0, 4094),
     }));
     return {
         code: 1,
         message: "success",
-        data
+        data,
     };
 });
 
@@ -65,17 +66,17 @@ Mock.mock("/switch_svp?form=svp_auto_profile_del", "post", "@BASESUCCESS");
 
 Mock.mock("/switch_svp?form=svp_auto_find", "get", () => {
     const n = Random.range(0, 8192);
-    const data = Array.from({ length: n }).map(item => ({
+    const data = Array.from({ length: n }).map((item, index) => ({
+        svp_af_id: index + 1,
         port_id: Random.range(1, 16),
         ont_id: Random.range(1, 128),
         gemport: Random.range(1, 5),
-        user_vlan: Random.range(1, 4094)
+        user_vlan: Random.range(1, 4094),
     }));
-    data.sort((a, b) => a.port_id - b.port_id);
     return {
         code: 1,
         message: "success",
-        data
+        data,
     };
 });
 
@@ -86,18 +87,18 @@ Mock.mock("/switch_svp?form=svp_auto_find", "get", () => {
 Mock.mock("/switch_svp?form=svp_stat_get", "get", () => {
     const n = Random.range(0, 8191);
     let i = 1;
-    const data = Array.from({ length: n }).map(item => ({
+    const data = Array.from({ length: n }).map((item) => ({
         svp_id: i++,
         stat_admin: Random.range(0, 1),
         ig_packets: Random.range(0, 1024000),
         ig_bytes: Random.range(0, 1024000),
         eg_packets: Random.range(0, 1024000),
-        eg_bytes: Random.range(0, 1024000)
+        eg_bytes: Random.range(0, 1024000),
     }));
     return {
         code: 1,
         message: "success",
-        data
+        data,
     };
 });
 
@@ -111,21 +112,23 @@ Mock.mock("/switch_svp?form=svp_stat_clear", "post", "@BASESUCCESS");
 
 Mock.mock("/switch_svp?form=traffic_profile_get", "get", () => {
     const n = Random.range(0, 256);
-    const bytes = _ => Random.range(1, 1024000);
+    const bytes = (_) => Random.range(1, 1024000);
     let i = 1;
-    const data = Array.from({ length: n }).map(item => ({
+    const data = Array.from({ length: n }).map((item) => ({
         traffic_profile_id: i++,
         cir: bytes(),
         cbs: bytes(),
         pir: bytes(),
-        pbs: bytes()
+        pbs: bytes(),
     }));
     return {
         code: 1,
         message: "success",
-        data
+        data,
     };
 });
 
 Mock.mock("/switch_svp?form=traffic_profile_set", "post", "@BASESUCCESS");
 Mock.mock("/switch_svp?form=svp_traffic_bind", "post", "@BASESUCCESS");
+
+Mock.post("/switch_svp?form=svp_af_create");
