ecd9ef3e26466dbff9f247224135dd72c3feea4e
stone-liu3432 <liushuang3432@163.com>
2020-06-06 11:18:48 +0800

    添加ont native vlan配置

--- src/views/advSetting/gponMgmt/ontPortConfig.vue
+++ src/views/advSetting/gponMgmt/ontPortConfig.vue
@@ -39,11 +39,37 @@
                         slot-scope="scope"
                     >{{ scope.row.ring === 1 ? $lang('on') : $lang('off') }}</template>
                 </el-table-column>
-                <el-table-column :label="$lang('na_vlan_id')" prop="na_vlan_id"></el-table-column>
-                <el-table-column :label="$lang('na_vlan_pri')" prop="na_vlan_pri"></el-table-column>
+                <el-table-column :label="$lang('na_vlan_id')" prop="na_vlan_id">
+                    <template
+                        slot-scope="scope"
+                    >{{ scope.row.na_vlan_id === 0x1000 ? '-' : scope.row.na_vlan_id }}</template>
+                </el-table-column>
+                <el-table-column :label="$lang('na_vlan_pri')" prop="na_vlan_pri">
+                    <template
+                        slot-scope="scope"
+                    >{{ (scope.row.na_vlan_id === 0x1000 || scope.row.na_vlan_pri > 7) ? '-' : scope.row.na_vlan_pri }}</template>
+                </el-table-column>
                 <el-table-column :label="$lang('config')">
                     <template slot-scope="scope">
-                        <el-button type="text" @click="openDialog(scope.row)">{{ $lang('config') }}</el-button>
+                        <el-dropdown @command="command">
+                            <span class="el-dropdown-link">
+                                <span>{{ $lang('config') }}</span>
+                                <i class="el-icon-arrow-down el-icon--right"></i>
+                            </span>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item
+                                    :command="{ action: 'port', data: scope.row }"
+                                >{{ $lang('config', 'port') }}</el-dropdown-item>
+                                <el-dropdown-item
+                                    :command="{ action: 'vlan', data: scope.row }"
+                                >{{$lang('config', 'na_vlan_id')}}</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                        <!-- <el-button type="text" @click="openDialog(scope.row)">{{ $lang('config') }}</el-button>
+                        <el-button
+                            type="text"
+                            @click="openDialog(scope.row)"
+                        >{{ $lang('na_vlan_id') }}</el-button>-->
                     </template>
                 </el-table-column>
             </el-table>
@@ -64,7 +90,7 @@
 
 <script>
 import { mapGetters } from "vuex";
-import { isArray, isDef } from "@/utils/common";
+import { isArray, isDef, isFunction } from "@/utils/common";
 import { UNI_TYPES } from "@/utils/commonData";
 import ontPortConfigForm from "./ontPortConfig/ontPortConfigForm";
 import postData from "@/mixin/postData";
@@ -119,32 +145,90 @@ export default {
             }
             this.getData(port_id, ont_id);
         },
-        openDialog(data) {
+        openDialog(type, data) {
             this.dialogVisible = true;
             this.$nextTick(() => {
-                this.$refs["ont-port-config-form"].init(data);
+                this.$refs["ont-port-config-form"].init(type, data);
             });
         },
         submitForm(formName) {
-            this.$refs[formName].validate(formData => {
-                if (isDef(formData)) {
-                    const url = "/gponont_mgmt?form=port_attr",
-                        post_params = {
-                            method: "set",
-                            param: formData
-                        };
-                    this.postData(url, post_params)
-                        .then(() => {
-                            this.getData(this.port_id, this.ont_id);
-                        })
-                        .catch(() => {});
-                    this.dialogVisible = false;
+            this.$refs[formName].validate((type, formData) => {
+                if (formData) {
+                    const ACTIONS = {
+                        port(form) {
+                            if (form.flags === 0) {
+                                this.$message.info(this.$lang("modify_tips"));
+                                return;
+                            }
+                            return {
+                                url: "/gponont_mgmt?form=port_attr",
+                                data: {
+                                    method: "set",
+                                    param: {
+                                        identifier: form.identifier,
+                                        uniport: form.uniport,
+                                        unitype: form.unitype,
+                                        flags: form.flags,
+                                        epspeed: form.epspeed,
+                                        epduplex: form.epduplex,
+                                        epautoneg: form.epautoneg,
+                                        adminstate: form.adminstate,
+                                        flow_ctrl: form.flow_ctrl,
+                                        ring: form.ring
+                                    }
+                                }
+                            };
+                        },
+                        vlan(form) {
+                            if (!form.flags) {
+                                this.$message.info(this.$lang("modify_tips"));
+                                return;
+                            }
+                            return {
+                                url: "/gponont_mgmt?form=natvie_vlan",
+                                data: {
+                                    method: "set",
+                                    param: {
+                                        identifier: form.identifier,
+                                        uniport: form.uniport,
+                                        unitype: form.unitype,
+                                        na_vlan_id: form.na_vlan_id,
+                                        na_vlan_pri: form.na_vlan_pri
+                                    }
+                                }
+                            };
+                        }
+                    };
+                    if (isFunction(ACTIONS[type])) {
+                        const res = ACTIONS[type].call(this, formData);
+                        if (res) {
+                            const { url, data } = res;
+                            url &&
+                                data &&
+                                this.postData(url, data)
+                                    .then(() => {
+                                        this.getData(this.port_id, this.ont_id);
+                                    })
+                                    .catch(() => {});
+                            this.dialogVisible = false;
+                        }
+                    }
                 }
             });
+        },
+        command({ action, data }) {
+            this.openDialog(action, data);
         }
     }
 };
 </script>
 
 <style lang="less" scoped>
+.el-dropdown-link {
+    cursor: pointer;
+    color: @titleColor;
+}
+.el-icon-arrow-down {
+    font-size: 12px;
+}
 </style>
--- src/views/advSetting/gponMgmt/ontPortConfig/ontPortConfigForm.vue
+++ src/views/advSetting/gponMgmt/ontPortConfig/ontPortConfigForm.vue
@@ -1,112 +1,197 @@
 <template>
-    <el-form :model="form" label-width="180px" ref="ont-port-config-form">
+    <el-form :model="form" label-width="180px" ref="ont-port-config-form" :rules="rules">
         <el-form-item :label="$lang('uniport')" prop="uniport">{{ form.uniport }}</el-form-item>
-        <el-form-item :label="$lang('epspeed')" prop="epspeed">
-            <el-select v-model.number="form.epspeed">
-                <el-option label="10" :value="10"></el-option>
-                <el-option label="100" :value="100"></el-option>
-                <el-option label="1000" :value="1000"></el-option>
-                <el-option label="Auto-neg" :value="0xffff"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item :label="$lang('epduplex')" prop="epduplex">
-            <el-select v-model.number="form.epduplex">
-                <el-option :label="$lang('full')" :value="0"></el-option>
-                <el-option :label="$lang('half')" :value="1"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item :label="$lang('epautoneg')" prop="epautoneg">
-            <el-select v-model.number="form.epautoneg">
-                <el-option :label="$lang('enable')" :value="0"></el-option>
-                <el-option :label="$lang('disable')" :value="1"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item :label="$lang('adminstate')" prop="adminstate">
-            <el-select v-model.number="form.adminstate">
-                <el-option :label="$lang('enable')" :value="0"></el-option>
-                <el-option :label="$lang('disable')" :value="1"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item :label="$lang('flow_ctrl')" prop="flow_ctrl">
-            <el-select v-model.number="form.flow_ctrl">
-                <el-option :label="$lang('enable')" :value="0"></el-option>
-                <el-option :label="$lang('disable')" :value="1"></el-option>
-            </el-select>
-        </el-form-item>
-        <el-form-item :label="$lang('ring')" prop="ring">
-            <el-select v-model.number="form.ring">
-                <el-option :label="$lang('on')" :value="1"></el-option>
-                <el-option :label="$lang('off')" :value="2"></el-option>
-            </el-select>
-        </el-form-item>
+        <template v-if="type === 'port'">
+            <el-form-item :label="$lang('epspeed')" prop="epspeed">
+                <el-select v-model.number="form.epspeed">
+                    <el-option label="10" :value="10"></el-option>
+                    <el-option label="100" :value="100"></el-option>
+                    <el-option label="1000" :value="1000"></el-option>
+                    <el-option label="Auto-neg" :value="0xffff"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('epduplex')" prop="epduplex">
+                <el-select v-model.number="form.epduplex">
+                    <el-option :label="$lang('full')" :value="0"></el-option>
+                    <el-option :label="$lang('half')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('epautoneg')" prop="epautoneg">
+                <el-select v-model.number="form.epautoneg">
+                    <el-option :label="$lang('enable')" :value="0"></el-option>
+                    <el-option :label="$lang('disable')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('adminstate')" prop="adminstate">
+                <el-select v-model.number="form.adminstate">
+                    <el-option :label="$lang('enable')" :value="0"></el-option>
+                    <el-option :label="$lang('disable')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('flow_ctrl')" prop="flow_ctrl">
+                <el-select v-model.number="form.flow_ctrl">
+                    <el-option :label="$lang('enable')" :value="0"></el-option>
+                    <el-option :label="$lang('disable')" :value="1"></el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('ring')" prop="ring">
+                <el-select v-model.number="form.ring">
+                    <el-option :label="$lang('on')" :value="1"></el-option>
+                    <el-option :label="$lang('off')" :value="2"></el-option>
+                </el-select>
+            </el-form-item>
+        </template>
+        <template v-if="type === 'vlan'">
+            <el-form-item :label="$lang('unitype')" prop="unitype" key="unitype">
+                <el-select v-model.number="form.unitype">
+                    <template v-for="(item,index) in UNI_TYPES">
+                        <el-option :label="item" :value="index"></el-option>
+                    </template>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$lang('na_vlan_id')" prop="na_vlan_id" key="na_vlan_id">
+                <el-input
+                    v-model.number="form.na_vlan_id"
+                    :disabled="flush_vlan"
+                    style="width: 215px;"
+                ></el-input>
+                <el-checkbox
+                    v-model="flush_vlan"
+                    style="margin-left: 30px;"
+                >{{ $lang('clear') }} VLAN</el-checkbox>
+            </el-form-item>
+            <el-form-item :label="$lang('na_vlan_pri')" prop="na_vlan_pri" key="na_vlan_pri">
+                <el-select v-model.number="form.na_vlan_pri" :disabled="flush_vlan">
+                    <el-option label="-" :value="15"></el-option>
+                    <el-option :value="0"></el-option>
+                    <template v-for="i in 7">
+                        <el-option :value="i"></el-option>
+                    </template>
+                </el-select>
+            </el-form-item>
+        </template>
     </el-form>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import { isDef, isFunction } from "@/utils/common";
+import { UNI_TYPES } from "@/utils/commonData";
+import { regRange } from "@/utils/validator";
 export default {
     name: "ontPortConfigForm",
     computed: {
-        ...mapGetters(["$lang"])
+        ...mapGetters(["$lang", "validateMsg"])
     },
     data() {
         return {
+            UNI_TYPES,
             form: {
+                identifier: 0,
                 uniport: "",
                 epspeed: 1000,
                 epduplex: 0,
                 epautoneg: 1,
                 adminstate: 1,
                 flow_ctrl: 1,
-                ring: 1
+                ring: 1,
+                unitype: 1,
+                na_vlan_id: "",
+                na_vlan_pri: 0
             },
-            flags: 0,
-            cacheData: null
+            rules: {
+                na_vlan_id: [
+                    {
+                        validator: this.validateVlanid,
+                        trigger: ["change", "blur"]
+                    }
+                ]
+            },
+            cacheData: null,
+            type: "",
+            flush_vlan: false
         };
     },
     methods: {
-        init(data) {
+        init(type, data) {
             this.$refs["ont-port-config-form"].resetFields();
-            this.flags = 0;
+            this.type = type;
+            this.flush_vlan = false;
             this.cacheData = JSON.parse(JSON.stringify(data));
             Object.keys(this.form).forEach(key => {
                 if (isDef(data[key])) {
                     this.form[key] = data[key];
                 }
             });
+            if (this.form.na_vlan_id === 0x1000) {
+                this.flush_vlan = true;
+            }
         },
         validate(fn) {
-            if (isFunction(fn)) {
-                if (
-                    Object.keys(this.form).every(
-                        key => this.form[key] === this.cacheData[key]
-                    )
-                ) {
-                    this.$message.info(this.$lang("modify_tips"));
-                    return fn.call(this);
-                }
-                const FLAGS = {
-                    epspeed: 0x1,
-                    epduplex: 0x1,
-                    flow_ctrl: 0x2,
-                    epautoneg: 0x4,
-                    adminstate: 0x8,
-                    ring: 0x10
-                };
-                const flags = Object.keys(FLAGS).reduce((prev, key) => {
-                    if (this.form[key] !== this.cacheData[key]) {
-                        if ((prev & FLAGS[key]) !== FLAGS[key]) {
-                            return prev | FLAGS[key];
+            this.$refs["ont-port-config-form"].validate(valid => {
+                if (isFunction(fn)) {
+                    if (valid) {
+                        let flags;
+                        if (this.type === "port") {
+                            const FLAGS = {
+                                epspeed: 0x1,
+                                epduplex: 0x1,
+                                flow_ctrl: 0x2,
+                                epautoneg: 0x4,
+                                adminstate: 0x8,
+                                ring: 0x10
+                            };
+                            flags = Object.keys(FLAGS).reduce((prev, key) => {
+                                if (this.form[key] !== this.cacheData[key]) {
+                                    if ((prev & FLAGS[key]) !== FLAGS[key]) {
+                                        return prev | FLAGS[key];
+                                    }
+                                }
+                                return prev;
+                            }, 0);
                         }
+                        if (this.type === "vlan") {
+                            if (this.flush_vlan) {
+                                this.form.na_vlan_id = 0x1000;
+                            }
+                            ["unitype", "na_vlan_id", "na_vlan_pri"].forEach(
+                                key => {
+                                    if (
+                                        this.form[key] !== this.cacheData[key]
+                                    ) {
+                                        flags = 1;
+                                    }
+                                }
+                            );
+                        }
+                        fn.call(this, this.type, {
+                            flags,
+                            ...this.form
+                        });
+                    } else {
+                        fn.call(this, this.type, valid);
                     }
-                    return prev;
-                }, 0);
-                fn.call(this, {
-                    flags,
-                    unitype: this.cacheData.unitype,
-                    ...this.form
-                });
+                }
+            });
+        },
+        validateVlanid(rule, val, cb) {
+            if (this.flush_vlan) {
+                return cb();
+            }
+            if (!regRange(val, 1, 4095)) {
+                return cb(new Error(this.validateMsg("inputRange", 1, 4095)));
+            }
+            cb();
+        }
+    },
+    watch: {
+        flush_vlan() {
+            if (this.flush_vlan) {
+                this.form.na_vlan_id = "";
+                this.form.na_vlan_pri = 15;
+            } else {
+                this.form.na_vlan_id = this.cacheData.na_vlan_id || "";
+                this.form.na_vlan_pri = this.cacheData.na_vlan_pri || 15;
             }
         }
     }
