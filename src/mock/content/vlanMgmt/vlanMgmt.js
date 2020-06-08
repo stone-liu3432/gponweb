d3a8bed836a5ef7482ce91a38844b1f62e7f0e02
stone-liu3432 <liushuang3432@163.com>
2020-06-08 09:33:51 +0800

    添加vlan管理的mock

---
+++ src/mock/content/vlanMgmt/vlanMgmt.js
@@ -0,0 +1,25 @@
+import Mock from "@/mock";
+
+const { Random } = Mock;
+
+Mock.mock("/switch_vlan", "get", "@BASESUCCESS");
+
+Mock.mock("/vlantable", "get", () => {
+    const data = Array.from({ length: Random.range(0, 4094) }).map(
+        (item, index) => ({
+            vlan_id: index + 1,
+            tagged_portlist: Random.portlist(),
+            untagged_portlist: Random.portlist(),
+            default_vlan_portlist: Random.portlist(),
+        })
+    );
+    return {
+        code: 1,
+        message: "success",
+        data,
+    };
+});
+
+Mock.post("/switch_vlan");
+
+Mock.post("/switch_vlanlist");
