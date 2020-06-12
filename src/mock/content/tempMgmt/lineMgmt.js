27ed9be1e130332b550f8f252b4f8d387071f0cf
stone-liu3432 <liushuang3432@163.com>
2020-06-12 09:27:30 +0800

    修改线路模板mock生成方式

--- src/mock/content/tempMgmt/lineMgmt.js
+++ src/mock/content/tempMgmt/lineMgmt.js
@@ -3,20 +3,17 @@ import Mock from "@/mock";
 const Random = Mock.Random;
 
 Mock.mock("/lineprofile?form=table", "get", () => {
-    const n = Random.range(1, 8);
-    const data = [];
-    for (let i = 0; i < n; i++) {
-        data.push({
+    const data = Array.from({ length: Random.range(0, 2048) }).map(
+        (item, index) => ({
             profname: Random.word(1, 32),
-            profid: Random.range(0, 2048),
-            bindtimes: Random.range(1, 8)
-        });
-    }
-    data.sort((a, b) => a.profid - b.profid);
+            profid: index,
+            bindtimes: Random.range(1, 8),
+        })
+    );
     return {
         code: 1,
         message: "success",
-        data
+        data,
     };
 });
 
@@ -40,8 +37,8 @@ Mock.mock(
                     {
                         mode: Random.pick([1, 3]),
                         vlan_id: 0xffff,
-                        vlan_pri: 0
-                    }
+                        vlan_pri: 0,
+                    },
                 ];
             }
             const mapping = [],
@@ -52,7 +49,7 @@ Mock.mock(
                 mapping.push({
                     mode,
                     vlan_id: Random.range(1, 4094),
-                    vlan_pri: Random.range(0, 7)
+                    vlan_pri: Random.range(0, 7),
                 });
             }
             mapping.sort((a, b) => a.vlan_id - b.vlan_id);
@@ -61,25 +58,25 @@ Mock.mock(
 
         for (let i = 0; i < n; i++) {
             const tcid = Random.range(1, 32);
-            if (tcont.some(item => item.tcid === tcid)) {
+            if (tcont.some((item) => item.tcid === tcid)) {
                 continue;
             }
             tcont.push({
                 tcid,
                 dba_profid: Random.range(0, 512),
-                dba_profname: Random.word(1, 32)
+                dba_profname: Random.word(1, 32),
             });
         }
         tcont.sort((a, b) => a.tcid - b.tcid);
         for (let i = 0; i < m; i++) {
             const gemindex = Random.range(1, 32);
-            if (gem.some(item => item.gemindex === gemindex)) {
+            if (gem.some((item) => item.gemindex === gemindex)) {
                 continue;
             }
             gem.push({
                 gemindex,
                 tcontid: Random.range(1, 32),
-                mapping: createMapping()
+                mapping: createMapping(),
             });
         }
         gem.sort((a, b) => a.gemindex - b.gemindex);
@@ -92,28 +89,28 @@ Mock.mock(
                 type: Random.range(1, 2),
                 mappingmode: Random.range(1, 3),
                 tcont,
-                gem
-            }
+                gem,
+            },
         };
     }
 );
 
 Mock.mock("/lineprofile", "post", "@BASESUCCESS");
 
-// generate random IP adress, and test RegExp
-// const generateIP = () => {
-//     return `${Random.range(0, 255)}.${Random.range(0, 255)}.${Random.range(
-//         0,
-//         255
-//     )}.${Random.range(0, 255)}`;
-// };
-
-// Array.from({ length: 99999 }).forEach(item => {
-//     const reg = /^((25[0-5]|2[0-4]\d|1?\d?\d)(\.(?!$)|$)){4}/;
-//     const ip = generateIP();
-//     if (!reg.test(ip)) {
-//         console.log("test fail, source IP is: ", ip);
-//     }
-// });
-
-// console.log("test done");
+Mock.mock(
+    /\/lineprofile\?form=boundinfo&profid=\d+&profname=\w+/,
+    "get",
+    ({ url }) => {
+        const data = Array.from({ length: Random.range(0, 16) }).map(
+            (item, index) => ({
+                port_id: index + 1,
+                resource: `0-${Random.range(1, 127)}`,
+            })
+        );
+        return {
+            code: 1,
+            message: "success",
+            data,
+        };
+    }
+);
