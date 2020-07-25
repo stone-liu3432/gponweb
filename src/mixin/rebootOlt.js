d25fe360c5d89062a60e6983ac4761f8397c77c7
stone-liu3432 <liushuang3432@163.com>
2020-07-25 17:09:12 +0800

    添加重启olt时跳转到login页

--- src/mixin/rebootOlt.js
+++ src/mixin/rebootOlt.js
@@ -1,25 +1,31 @@
 "use strict";
+import { clearSessionStorage, isFunction } from "@/utils/common";
 
 export default {
     methods: {
         rebootOlt(tips, timer) {
-            const t = tips || this.$lang("reboot_olt_hit");
-            return new Promise((resolve, reject) => {
-                this.$confirm(t)
-                    .then(_ => {
-                        this.reboot();
-                        resolve();
-                    })
-                    .catch(_ => {
-                        clearTimeout(timer);
-                    });
-            });
+            const msg = tips || this.$lang("reboot_olt_hit");
+            return this.$confirm(msg)
+                .then(() => {
+                    this.reboot();
+                    return Promise.resolve();
+                })
+                .catch(() => {
+                    clearTimeout(timer);
+                });
         },
         reboot() {
             this.$http
                 .get("/system_reboot")
-                .then(res => {})
-                .catch(err => {});
-        }
-    }
+                .then((res) => {})
+                .catch((err) => {});
+            clearSessionStorage();
+            this.$router.push("/login");
+            // 特殊处理，在打开confirm的情况下自动完成跳转到登录页时，关闭confirm
+            const el = document.querySelector(".el-message-box__wrapper");
+            if (el && isFunction(el.click)) {
+                el.click();
+            }
+        },
+    },
 };
