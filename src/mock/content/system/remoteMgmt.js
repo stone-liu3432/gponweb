971b8ba42b51d2a1988b729d401db5c47734037c
stone-liu3432 <liushuang3432@163.com>
2020-08-31 17:14:05 +0800

    添加dns信息

--- src/mock/content/system/remoteMgmt.js
+++ src/mock/content/system/remoteMgmt.js
@@ -2,6 +2,29 @@
 
 import Mock from "@/mock";
 
+const { Random } = Mock;
+
 Mock.mock("/system?form=inbound", "post", "@BASESUCCESS");
 
 Mock.mock("/system?form=outbound", "post", "@BASESUCCESS");
+
+Mock.mock("/switch_route?form=route_default", "get", () => ({
+    code: 1,
+    message: "success",
+    data: {
+        gateway: Random.ip(),
+    },
+}));
+
+Mock.post("/switch_route?form=route_default");
+
+Mock.mock("/system?form=dns", "get", () => ({
+    code: 1,
+    message: "success",
+    data: {
+        primary: Random.ip(),
+        secondary: Random.ip(),
+    },
+}));
+
+Mock.post("/system?form=dns");
