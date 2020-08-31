1b5c91bc903d298f828572b9865101388dac73a2
stone-liu3432 <liushuang3432@163.com>
2020-08-31 17:14:36 +0800

    添加系统端口配置

--- src/mock/content/system/service.js
+++ src/mock/content/system/service.js
@@ -13,8 +13,8 @@ Mock.mock("/system_service?form=frpc", "get", {
         local_ip: "127.0.0.1",
         local_port: 80,
         remote_port: "@RANGE(1,65535)",
-        custom_domains: "www.test.com"
-    }
+        custom_domains: "www.test.com",
+    },
 });
 
 Mock.mock("/snmp_cfg?form=trap", "get", {
@@ -23,8 +23,8 @@ Mock.mock("/snmp_cfg?form=trap", "get", {
     data: {
         serverip: "@IP",
         trap_port: "@RANGE(1,65535)",
-        trap_community: Random.first()
-    }
+        trap_community: Random.first(),
+    },
 });
 
 Mock.mock("/snmp_cfg?form=community", "get", {
@@ -32,8 +32,8 @@ Mock.mock("/snmp_cfg?form=community", "get", {
     message: "success",
     data: {
         read_community: Random.first(),
-        write_community: Random.last()
-    }
+        write_community: Random.last(),
+    },
 });
 
 Mock.mock("/system_service?form=sshd", "get", {
@@ -43,9 +43,9 @@ Mock.mock("/system_service?form=sshd", "get", {
         {
             keyname: Random.first(),
             publickey:
-                "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFkr4tNiHUKTlNNJuM7JeAXN5VibwxYUMtGwzYtZIDUnyYqak2Bj4ByIeNn+dLZVAQw7fYoLJzVpmaMhwcCtgXwhFy1DDzXD2UsU2GOtMSSQlXi1hV8Eukc6GDggc077LV8T3hPCWcddthVxywn0jp3VrXnI0KQ7ZkRDiwDj3TcSVvTjyFPnsLKm3gwWRrX7mHJjV53lZty1DIBJ74GwzvBgAJHfY6ly3vxKAmJ7uKALgugRyK12vYUon64G2XqEh1r8n5elmbu6jy1v6DsrNUF2ZB29MXA0pWWPuyRmcqy7PFtVHbbQK4hJlPGyUTcXdDPfCygxBleGo9root@localhost.localdomain"
-        }
-    ]
+                "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFkr4tNiHUKTlNNJuM7JeAXN5VibwxYUMtGwzYtZIDUnyYqak2Bj4ByIeNn+dLZVAQw7fYoLJzVpmaMhwcCtgXwhFy1DDzXD2UsU2GOtMSSQlXi1hV8Eukc6GDggc077LV8T3hPCWcddthVxywn0jp3VrXnI0KQ7ZkRDiwDj3TcSVvTjyFPnsLKm3gwWRrX7mHJjV53lZty1DIBJ74GwzvBgAJHfY6ly3vxKAmJ7uKALgugRyK12vYUon64G2XqEh1r8n5elmbu6jy1v6DsrNUF2ZB29MXA0pWWPuyRmcqy7PFtVHbbQK4hJlPGyUTcXdDPfCygxBleGo9root@localhost.localdomain",
+        },
+    ],
 });
 
 Mock.mock("/system_service?form=frpc", "post", "@BASESUCCESS");
@@ -55,3 +55,15 @@ Mock.mock("/snmp_cfg?form=community", "post", "@BASESUCCESS");
 Mock.mock("/snmp_cfg?form=trap", "post", "@BASESUCCESS");
 
 Mock.mock("/system_service?form=sshd", "post", "@BASESUCCESS");
+
+Mock.mock("/system_service?form=port", "get", () => ({
+    code: 1,
+    message: "success",
+    data: {
+        http: Random.range(1, 65535),
+        https: Random.range(1, 65535),
+        telnet: Random.range(1, 65535),
+    },
+}));
+
+Mock.post("/system_service?form=port");
