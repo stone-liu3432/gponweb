d92318febd2b5df6474ff43906d870e597a17061
stone-liu3432 <liushuang3432@163.com>
2020-09-04 10:01:16 +0800

    添加ont告警查看页

---
+++ src/views/advSetting/gponMgmt/ontBasicInfo/ontAlarm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <template v-for="item in data">
+                <li>{{ item }}</li>
+            </template>
+        </ul>
+        <el-backtop :target="backTopTarget"></el-backtop>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ontAlarm",
+    props: {
+        data: {
+            type: Array
+        }
+    },
+    inject: ["backTopTarget"]
+};
+</script>
+
+<style lang="less" scoped>
+li {
+    .base-font-style;
+    padding: 6px 0;
+    font-size: 16px;
+}
+</style>
