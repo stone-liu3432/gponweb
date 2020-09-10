7e17a48d67b3f4e53960f6e3831f9fe6a77d63f9
stone-liu3432 <liushuang3432@163.com>
2020-09-10 11:15:06 +0800

    添加批量添加到认证列表，及批量删除功能

--- src/views/page/onuAllow/ontAutoFind.vue
+++ src/views/page/onuAllow/ontAutoFind.vue
@@ -1,12 +1,24 @@
 <template>
     <div>
-        <div style="margin: 10px 0 20px 10px;">
+        <div style="margin: 10px 0 20px 0;">
+            <nms-filter inline :data="autofindList" :primary="filterable" @change="dataChange"></nms-filter>
             <el-button type="primary" size="small" @click="changeBatch">
                 <template v-if="!isBatch">{{ $lang('batch_config') }}</template>
                 <template v-else>{{ $lang('exit_batch_onu') }}</template>
             </el-button>
             <template v-if="isBatch">
-                <el-button type="primary" size="small" @click="submitBatch">{{ $lang('delete') }}</el-button>
+                <el-button
+                    type="primary"
+                    size="small"
+                    style="margin-left: 30px;"
+                    @click="submitBatch"
+                >{{ $lang('delete') }}</el-button>
+                <el-button
+                    type="primary"
+                    size="small"
+                    style="margin-left: 30px;"
+                    @click="batchAddToAuthlist"
+                >{{ $lang('add_to_auth_list') }}</el-button>
             </template>
         </div>
         <el-table
@@ -22,7 +34,7 @@
             <el-table-column :label="$lang('ont_id')">
                 <template
                     slot-scope="scope"
-                >{{ `${getPortName((scope.row.identifier >> 8) & 0xff)}/${(scope.row.identifier) & 0xff}` }}</template>
+                >{{ `${getPortName((scope.row.identifier >> 8) & 0xff)}/${scope.row.ont_id}` }}</template>
             </el-table-column>
             <el-table-column :label="$lang('ont_sn')" prop="ont_sn"></el-table-column>
             <el-table-column :label="$lang('ont_password')" prop="ont_password"></el-table-column>
@@ -48,9 +60,20 @@
             :page-sizes="[10, 20, 30, 50]"
             :page-size.sync="pageSize"
             layout="total, sizes, prev, pager, next, jumper"
-            :total="autofindList.length"
+            :total="filterableList.length"
             hide-on-single-page
         ></el-pagination>
+        <el-dialog :visible="dialogVisible" append-to-body width="650px">
+            <div slot="title">{{ $lang('config') }}</div>
+            <auto-find-form ref="auto-find-form"></auto-find-form>
+            <div slot="footer">
+                <el-button @click="dialogVisible = false;">{{ $lang('cancel') }}</el-button>
+                <el-button
+                    type="primary"
+                    @click="submitBatchAdd('auto-find-form')"
+                >{{ $lang('apply') }}</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -58,13 +81,39 @@
 import { mapGetters } from "vuex";
 import { isArray } from "@/utils/common";
 import postData from "@/mixin/postData";
+import autoFindForm from "./autoFindForm";
 export default {
     name: "ontAutoFind",
+    components: { autoFindForm },
     computed: {
         ...mapGetters(["$lang", "getPortName"]),
         showTable() {
             const start = (this.currentPage - 1) * this.pageSize;
-            return this.autofindList.slice(start, start + this.pageSize);
+            return this.filterableList.slice(start, start + this.pageSize);
+        },
+        filterable() {
+            return [
+                {
+                    prop: "ont_id",
+                    value: 1,
+                    label: this.$lang("ont_id")
+                },
+                {
+                    prop: "ont_sn",
+                    value: 2,
+                    label: this.$lang("ont_sn")
+                },
+                {
+                    prop: "loid",
+                    value: 3,
+                    label: this.$lang("loid")
+                },
+                {
+                    prop: "vendorid",
+                    value: 4,
+                    label: this.$lang("vendorid")
+                }
+            ];
         }
     },
     mixins: [postData],
@@ -80,7 +129,9 @@ export default {
             currentPage: 1,
             pageSize: 10,
             isBatch: false,
-            ontlist: []
+            ontlist: [],
+            dialogVisible: false,
+            filterableList: []
         };
     },
     inject: ["updateNavScrollbar"],
@@ -105,6 +156,10 @@ export default {
                 .then(res => {
                     if (res.data.code === 1) {
                         if (isArray(res.data.data)) {
+                            // const data = res.data.data.map(item => {
+                            //     //item.ont_id = item.identifier & 0xff;
+                            //     return item;
+                            // });
                             this.autofindList = res.data.data;
                         }
                     }
@@ -156,6 +211,48 @@ export default {
                     this.changeBatch();
                 })
                 .catch(_ => {});
+        },
+        batchAddToAuthlist() {
+            if (!this.ontlist.length) {
+                return this.$message.info(this.$lang("modify_tips"));
+            }
+            this.$confirm(this.$lang("batch_add_to_authlist_tips"))
+                .then(_ => {
+                    this.dialogVisible = true;
+                    this.$nextTick(_ => {
+                        this.$refs["auto-find-form"].init();
+                    });
+                })
+                .catch(_ => {});
+        },
+        submitBatchAdd(formName) {
+            this.$refs[formName].validate(form => {
+                const data = this.ontlist.map(item =>
+                    item.ont_sn.slice(0, item.ont_sn.indexOf("("))
+                );
+                this.postData(
+                    "/gponont_mgmt?form=multiauth&port_id=" + this.port_id,
+                    {
+                        method: "add",
+                        param: {
+                            ont_lineprofid: form.ont_lineprofid,
+                            ont_lineprofname: "",
+                            ont_srvprofid: form.ont_srvprofid,
+                            ont_srvprofname: "",
+                            ont_sn: data
+                        }
+                    }
+                )
+                    .then(_ => {
+                        this.getData(this.port_id);
+                    })
+                    .catch(_ => {});
+                this.dialogVisible = false;
+                this.isBatch = false;
+            });
+        },
+        dataChange(data) {
+            this.filterableList = data;
         }
     },
     watch: {
