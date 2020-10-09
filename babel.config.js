e65f978babde3d2276c35465031e7a8bdf29200a
stone-liu3432 <liushuang3432@163.com>
2020-10-09 17:07:23 +0800

    修改css为按需引入

--- babel.config.js
+++ babel.config.js
@@ -6,7 +6,7 @@ module.exports = {
             "component",
             {
                 libraryName: "element-ui",
-                styleLibraryName: "theme-chalk",
+                styleLibraryName: "~theme",
             },
         ],
     ],
