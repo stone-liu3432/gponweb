ea6d1b35dc5a4bfae5bb7a1c4793deb12f53cfb8
stone-liu3432 <liushuang3432@163.com>
2020-10-09 10:59:08 +0800

    添加删除ONT端口VLAN功能

--- src/views/advSetting/gponMgmt/opVlan.vue
+++ src/views/advSetting/gponMgmt/opVlan.vue
@@ -1,48 +1,81 @@
 <template>
     <div>
         <page-header type="pon" hasOnu @port-change="portChange">
-            <template slot="title">{{ $lang('op_vlan') }}</template>
+            <template slot="title">{{ $lang("op_vlan") }}</template>
         </page-header>
         <div style="margin: 12px 0;" v-if="ont_id !== 0xffff">
-            <el-button type="primary" size="small" @click="openDialog('add')">{{ $lang('add') }}</el-button>
+            <el-button type="primary" size="small" @click="openDialog('add')">{{
+                $lang("add")
+            }}</el-button>
         </div>
-        <template v-if="ont_id !== 0xffff && vlanList.portvlan && vlanList.portvlan.length">
+        <template v-if="ont_id !== 0xffff">
             <el-table :data="vlanList.portvlan || []" border>
-                <el-table-column :label="$lang('uniport')" prop="uniport"></el-table-column>
+                <el-table-column
+                    :label="$lang('uniport')"
+                    prop="uniport"
+                ></el-table-column>
                 <el-table-column :label="$lang('unitype')">
-                    <template slot-scope="scope">{{ UNI_TYPES[scope.row.unitype] || '' }}</template>
+                    <template slot-scope="scope">{{
+                        UNI_TYPES[scope.row.unitype] || ""
+                    }}</template>
                 </el-table-column>
                 <el-table-column :label="$lang('mode')">
-                    <template slot-scope="scope">{{ VLAN_MODES[scope.row.mode] || '' }}</template>
+                    <template slot-scope="scope">{{
+                        VLAN_MODES[scope.row.mode] || ""
+                    }}</template>
                 </el-table-column>
-                <el-table-column :label="$lang('svlanid')" prop="svlanid"></el-table-column>
+                <el-table-column
+                    :label="$lang('svlanid')"
+                    prop="svlanid"
+                ></el-table-column>
                 <el-table-column :label="$lang('svlanpri')" prop="svlanpri">
-                    <template
-                        slot-scope="scope"
-                    >{{ scope.row.svlanpri > 7 ? '-' : scope.row.svlanpri }}</template>
+                    <template slot-scope="scope">{{
+                        scope.row.svlanpri > 7 ? "-" : scope.row.svlanpri
+                    }}</template>
                 </el-table-column>
-                <el-table-column :label="$lang('cvlanid')" prop="cvlanid"></el-table-column>
+                <el-table-column
+                    :label="$lang('cvlanid')"
+                    prop="cvlanid"
+                ></el-table-column>
                 <el-table-column :label="$lang('cvlanpri')" prop="cvlanpri">
-                    <template
-                        slot-scope="scope"
-                    >{{ scope.row.cvlanpri > 7 ? '-' : scope.row.cvlanpri }}</template>
+                    <template slot-scope="scope">{{
+                        scope.row.cvlanpri > 7 ? "-" : scope.row.cvlanpri
+                    }}</template>
                 </el-table-column>
                 <el-table-column :label="$lang('config')">
                     <template slot-scope="scope">
                         <el-button
                             type="text"
                             @click="openDialog('config', scope.row)"
-                        >{{ $lang('config') }}</el-button>
+                            >{{ $lang("config") }}</el-button
+                        >
+                        <el-button
+                            type="text"
+                            @click="deleteOpvlan(scope.row)"
+                            >{{ $lang("delete") }}</el-button
+                        >
                     </template>
                 </el-table-column>
             </el-table>
         </template>
-        <el-dialog :visible.sync="dialogVisible" append-to-body width="650px" destroy-on-close>
+        <el-dialog
+            :visible.sync="dialogVisible"
+            append-to-body
+            width="650px"
+            destroy-on-close
+        >
             <div slot="title">{{ $lang(dialogType) }}</div>
-            <op-vlan-form ref="op-vlan-form" :data="vlanList.portvlan || []"></op-vlan-form>
+            <op-vlan-form
+                ref="op-vlan-form"
+                :data="vlanList.portvlan || []"
+            ></op-vlan-form>
             <div slot="footer">
-                <el-button @click="dialogVisible = false;">{{ $lang('cancel') }}</el-button>
-                <el-button type="primary" @click="submitForm('op-vlan-form')">{{ $lang('apply') }}</el-button>
+                <el-button @click="dialogVisible = false">{{
+                    $lang("cancel")
+                }}</el-button>
+                <el-button type="primary" @click="submitForm('op-vlan-form')">{{
+                    $lang("apply")
+                }}</el-button>
             </div>
         </el-dialog>
     </div>
@@ -133,6 +166,29 @@ export default {
                     this.dialogVisible = false;
                 }
             });
+        },
+        deleteOpvlan(row) {
+            this.$confirm(this.$lang("if_sure", "delete") + " ?")
+                .then(_ => {
+                    this.postData("/gponont_mgmt?form=port_vlan", {
+                        method: "delete",
+                        param: {
+                            identifier: (this.port_id << 8) | this.ont_id,
+                            uniport: row.uniport,
+                            unitype: row.unitype,
+                            mode: row.mode,
+                            svlanid: row.svlanid,
+                            svlanpri: row.svlanpri,
+                            cvlanid: row.cvlanid,
+                            cvlanpri: row.cvlanpri
+                        }
+                    })
+                        .then(_ => {
+                            this.getData(this.port_id, this.ont_id);
+                        })
+                        .catch(_ => {});
+                })
+                .catch(_ => {});
         }
     }
 };
