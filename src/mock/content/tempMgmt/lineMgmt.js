cb1da203797f8ff5a898a8773bc10d75351ec3ab
stone-liu3432 <liushuang3432@163.com>
2020-11-09 17:01:04 +0800

    修改线路模板的获取方式

--- src/mock/content/tempMgmt/lineMgmt.js
+++ src/mock/content/tempMgmt/lineMgmt.js
@@ -2,7 +2,9 @@ import Mock from "@/mock";
 
 const Random = Mock.Random;
 
-Mock.mock("/lineprofile?form=table", "get", () => {
+Mock.mock("/lineprofile?form=table", "get", "@BASESUCCESS");
+
+Mock.mock("/ont_line_prof_table", "get", () => {
     const data = Array.from({ length: Random.range(0, 2048) }).map(
         (item, index) => ({
             profname: Random.word(1, 32),
