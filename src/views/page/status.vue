edd26aa98ab23d5de4ecfd2b9996fc37b2f61d63
stone-liu3432 <liushuang3432@163.com>
2020-11-11 10:22:27 +0800

    修改页面刷新方式，切换tab时立即刷新一次数据

--- src/views/page/status.vue
+++ src/views/page/status.vue
@@ -3,23 +3,31 @@
         <el-tabs v-model="activeName" type="card">
             <el-tab-pane label="PON" name="pon">
                 <div class="ont-status-statistics">
-                    <span>{{ $lang('registered_onu') }}:</span>
+                    <span>{{ $lang("registered_onu") }}:</span>
                     <span>{{ online + offline }}</span>
-                    <span style="margin-left: 30px;">{{ $lang('online') }}:</span>
+                    <span style="margin-left: 30px;"
+                        >{{ $lang("online") }}:</span
+                    >
                     <span>{{ online }}</span>
-                    <span style="margin-left: 30px;color: #F56C6C;">{{ $lang('offline') }}:</span>
+                    <span style="margin-left: 30px;color: #F56C6C;"
+                        >{{ $lang("offline") }}:</span
+                    >
                     <span style="color: #F56C6C;">{{ offline }}</span>
                 </div>
                 <el-row :gutter="40">
                     <template v-for="item in pon">
                         <el-col :span="6" style="margin: 20px 0;">
-                            <port-card type="pon" :data="item" @click.native="clickItem(item)"></port-card>
+                            <port-card
+                                type="pon"
+                                :data="item"
+                                @click.native="clickItem(item)"
+                            ></port-card>
                         </el-col>
                     </template>
                 </el-row>
             </el-tab-pane>
             <el-tab-pane :label="$lang('uplink')" name="uplink">
-                <el-row :gutter="40">
+                <el-row :gutter="30">
                     <template v-for="item in port">
                         <template v-if="item.port_id > system.ponports">
                             <el-col :span="6" style="margin: 20px 0;">
@@ -78,10 +86,7 @@ export default {
     },
     mounted() {
         // 定时刷新数据
-        const interval = setInterval(_ => {
-            this.activeName === "pon" && this.getPon();
-            this.activeName === "uplink" && this.getPort();
-        }, 5000);
+        const interval = setInterval(this.refreshData, 5000);
         this.$once("hook:beforeDestroy", _ => {
             clearInterval(interval);
         });
@@ -104,6 +109,15 @@ export default {
                     query: { port_id: row.port_id }
                 });
             }
+        },
+        refreshData() {
+            this.activeName === "pon" && this.getPon();
+            this.activeName === "uplink" && this.getPort();
+        }
+    },
+    watch: {
+        activeName() {
+            this.refreshData();
         }
     }
 };
