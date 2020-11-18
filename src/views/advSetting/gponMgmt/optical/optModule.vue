2036f6bf6d6520a71c1dbe6537749edf296965b1
stone-liu3432 <liushuang3432@163.com>
2020-11-18 16:49:15 +0800

    format

--- src/views/advSetting/gponMgmt/optical/optModule.vue
+++ src/views/advSetting/gponMgmt/optical/optModule.vue
@@ -5,15 +5,25 @@
                 <opt-card :data="mstateData" title="o_m_state"></opt-card>
             </el-col>
             <el-col :span="12">
-                <opt-card :data="manuInfoData" title="o_m_maunu_info"></opt-card>
+                <opt-card
+                    :data="manuInfoData"
+                    title="o_m_maunu_info"
+                ></opt-card>
             </el-col>
         </el-row>
-        <el-row :gutter="20" style="margin-top: 20px;">
+        <el-row :gutter="20" style="margin-top: 20px">
             <el-col :span="12">
-                <opt-card :data="staticInfoData" title="o_m_static_info"></opt-card>
+                <opt-card
+                    :data="staticInfoData"
+                    title="o_m_static_info"
+                ></opt-card>
             </el-col>
             <el-col :span="12">
-                <opt-card :data="opParamData" title="o_m_threshold" :layout="[16, 8]"></opt-card>
+                <opt-card
+                    :data="opParamData"
+                    title="o_m_threshold"
+                    :layout="[16, 8]"
+                ></opt-card>
             </el-col>
         </el-row>
     </div>
@@ -32,7 +42,7 @@ export default {
             const STATE_MAP = {
                 portstate: [this.$lang("offline"), this.$lang("online")],
                 mstate: [this.$lang("undetected"), this.$lang("detected")],
-                mtype: [this.$lang("in_cali"), this.$lang("ex_cali")]
+                mtype: [this.$lang("in_cali"), this.$lang("ex_cali")],
             };
             return this.mStates.reduce((prev, item) => {
                 if (!prev[item]) {
@@ -50,7 +60,7 @@ export default {
                     prev[item] = this.data[item];
                     if (isArray(this.data[item])) {
                         prev[item] = this.data[item]
-                            .map(i => i.toString(16))
+                            .map((i) => i.toString(16))
                             .join(" ");
                     }
                 }
@@ -72,12 +82,12 @@ export default {
                 )}, ${this.data[this.opParams[item][1]].toFixed(2)}]`;
                 return prev;
             }, {});
-        }
+        },
     },
     props: {
         data: {
-            type: Object
-        }
+            type: Object,
+        },
     },
     data() {
         return {
@@ -88,7 +98,7 @@ export default {
                 "work_temprature",
                 "work_voltage",
                 "transmit_bias",
-                "transmit_power"
+                "transmit_power",
                 // "receive_power"
             ],
             manuInfos: [
@@ -98,7 +108,7 @@ export default {
                 "vendor_pn",
                 "vendor_sn",
                 "date_code",
-                "vendor_specific"
+                "vendor_specific",
             ],
             staticInfos: [
                 "module_type",
@@ -108,7 +118,7 @@ export default {
                 "identifier",
                 "connector",
                 "encoding",
-                "cc_base"
+                "cc_base",
             ],
             opParams: {
                 temp_warn_threshold: ["temp_low_warning", "temp_high_warning"],
@@ -117,29 +127,29 @@ export default {
                 vol_alarm_threshold: ["vol_low_alarm", "vol_high_alarm"],
                 tx_bias_warn_threshold: [
                     "bias_low_warning",
-                    "bias_high_warning"
+                    "bias_high_warning",
                 ],
                 tx_bias_alarm_threshold: ["bias_low_alarm", "bias_high_alarm"],
                 tx_power_warn_threshold: [
                     "txpwr_low_warning",
-                    "txpwr_high_warning"
+                    "txpwr_high_warning",
                 ],
                 tx_power_alarm_threshold: [
                     "txpwr_low_alarm",
-                    "txpwr_high_alarm"
+                    "txpwr_high_alarm",
                 ],
                 rx_power_warn_threshold: [
                     "rxpwr_low_warning",
-                    "rxpwr_high_warning"
+                    "rxpwr_high_warning",
                 ],
                 rx_power_alarm_threshold: [
                     "rxpwr_low_alarm",
-                    "rxpwr_high_alarm"
-                ]
-            }
+                    "rxpwr_high_alarm",
+                ],
+            },
         };
     },
-    methods: {}
+    methods: {},
 };
 </script>
 
