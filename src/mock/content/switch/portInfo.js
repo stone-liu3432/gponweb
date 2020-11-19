7993295145ef52166218edc91a679f4d2f8728ba
stone-liu3432 <liushuang3432@163.com>
2020-11-19 14:54:38 +0800

    update

--- src/mock/content/switch/portInfo.js
+++ src/mock/content/switch/portInfo.js
@@ -3,8 +3,8 @@ import Mock from "@/mock";
 const { Random } = Mock;
 
 Mock.mock("/switch_port?form=optical_uplinkinfo", "get", () => {
-    const data = Array.from({ length: 24 }).map((item, index) => ({
-        port_id: 1 + index,
+    const data = Array.from({ length: 8 }).map((item, index) => ({
+        port_id: 17 + index,
         portstate: Random.range(0, 1),
         mstate: Random.range(0, 1),
         work_temprature: `${Math.floor(Math.random() * 50)} C`,
@@ -19,3 +19,40 @@ Mock.mock("/switch_port?form=optical_uplinkinfo", "get", () => {
         data,
     };
 });
+
+Mock.mock(
+    /\/switch_port\?form=optical_uplinkinfo&port_id=\d+/,
+    "get",
+    ({ url }) => {
+        const port_id = url.match(/(?<=port_id=)\d+/) >>> 0;
+        return {
+            code: 1,
+            message: "success",
+            data: {
+                port_id,
+                portstate: Random.range(0, 1),
+                mstate: Random.range(0, 1),
+                mtype: Random.range(0, 1),
+                work_temprature: "21 C",
+                work_voltage: "3.29 V",
+                transmit_bias: "13 mA",
+                transmit_power: "1.8295 dBm",
+                receive_power: "1.8295 dBm",
+                vendor_name: "ODI",
+                vendor_oui: "unspecify",
+                vendor_rev: "1.0",
+                vendor_pn: "LTE4302M-BC+HW",
+                vendor_sn: "F9842018765",
+                date_code: "18-09-03",
+                vendor_specific: [],
+                module_type: "EPON",
+                max_distance: 20,
+                wave_length: 1490,
+                fiber_type: "Single Mode",
+                identifier: "SFP or SFP Plus",
+                connector: "SC",
+                encoding: "8B10B",
+            },
+        };
+    }
+);
