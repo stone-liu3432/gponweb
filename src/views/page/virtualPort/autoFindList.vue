7881510b645a7cd3224f7393469b5c7238c37419
stone-liu3432 <liushuang3432@163.com>
2021-03-13 10:37:38 +0800

    vlan添加untag和transparent模式

--- src/views/page/virtualPort/autoFindList.vue
+++ src/views/page/virtualPort/autoFindList.vue
@@ -1,30 +1,50 @@
 <template>
     <div>
-        <div style="margin: 12px;">
-            <el-button type="primary" size="small" @click="refreshData">{{ $lang('refresh') }}</el-button>
+        <div style="margin: 12px">
+            <el-button type="primary" size="small" @click="refreshData">{{
+                $lang("refresh")
+            }}</el-button>
         </div>
         <el-table :data="showList" border>
-            <el-table-column :label="$lang('svp_af_id')" prop="svp_af_id"></el-table-column>
+            <el-table-column
+                :label="$lang('svp_af_id')"
+                prop="svp_af_id"
+            ></el-table-column>
             <el-table-column :label="$lang('port_id')">
-                <template slot-scope="scope">{{ getPortName(scope.row.port_id) }}</template>
+                <template slot-scope="scope">{{
+                    getPortName(scope.row.port_id)
+                }}</template>
             </el-table-column>
-            <el-table-column :label="$lang('ont_id')" prop="ont_id"></el-table-column>
-            <el-table-column :label="$lang('gemport')" prop="gemport"></el-table-column>
+            <el-table-column
+                :label="$lang('ont_id')"
+                prop="ont_id"
+            ></el-table-column>
+            <el-table-column
+                :label="$lang('gemport')"
+                prop="gemport"
+            ></el-table-column>
             <el-table-column :label="$lang('user_vlan')" prop="user_vlan">
-                <template slot-scope="scope">{{ scope.row.user_vlan || '-' }}</template>
+                <template slot-scope="scope">
+                    {{
+                        scope.row.user_vlan === 0
+                            ? "untag"
+                            : scope.row.user_vlan === 0xfffe
+                            ? "transparent"
+                            : scope.row.user_vlan
+                    }}
+                </template>
             </el-table-column>
             <el-table-column :label="$lang('config')">
                 <template slot-scope="scope">
-                    <el-button
-                        type="text"
-                        @click="openDialog(scope.row)"
-                    >{{ $lang('add', 'virtual_port') }}</el-button>
+                    <el-button type="text" @click="openDialog(scope.row)">{{
+                        $lang("add", "virtual_port")
+                    }}</el-button>
                 </template>
             </el-table-column>
         </el-table>
         <el-pagination
             :current-page.sync="currentPage"
-            style="float: right; margin: 12px 0;"
+            style="float: right; margin: 12px 0"
             :page-sizes="[10, 20, 30, 50]"
             :page-size.sync="pageSize"
             layout="total, sizes, prev, pager, next, jumper"
@@ -32,14 +52,19 @@
             hide-on-single-page
         ></el-pagination>
         <el-dialog :visible.sync="dialogVisible" width="640px">
-            <span slot="title">{{ $lang('add') }}</span>
-            <add-vp-from-af-form ref="add-vp-from-af-form"></add-vp-from-af-form>
+            <span slot="title">{{ $lang("add") }}</span>
+            <add-vp-from-af-form
+                ref="add-vp-from-af-form"
+            ></add-vp-from-af-form>
             <div slot="footer">
-                <el-button @click="dialogVisible = false;">{{ $lang('cancel') }}</el-button>
+                <el-button @click="dialogVisible = false">{{
+                    $lang("cancel")
+                }}</el-button>
                 <el-button
                     type="primary"
                     @click="submitForm('add-vp-from-af-form')"
-                >{{ $lang('apply') }}</el-button>
+                    >{{ $lang("apply") }}</el-button
+                >
             </div>
         </el-dialog>
     </div>
@@ -59,11 +84,11 @@ export default {
         showList() {
             const start = (this.currentPage - 1) * this.pageSize;
             return this.findList.slice(start, start + this.pageSize);
-        }
+        },
     },
     inject: ["updateNavScrollbar"],
     updated() {
-        this.$nextTick(_ => {
+        this.$nextTick((_) => {
             this.updateNavScrollbar();
         });
     },
@@ -72,7 +97,7 @@ export default {
             findList: [],
             currentPage: 1,
             pageSize: 10,
-            dialogVisible: false
+            dialogVisible: false,
         };
     },
     created() {
@@ -83,30 +108,30 @@ export default {
             this.findList = [];
             this.$http
                 .get("/switch_svp?form=svp_auto_find")
-                .then(res => {
+                .then((res) => {
                     if (res.data.code === 1) {
                         this.$http
                             .get("/serviceport_af")
-                            .then(res => {
+                            .then((res) => {
                                 if (res.data.code === 1) {
                                     if (isArray(res.data.data)) {
                                         this.findList = res.data.data;
                                     }
                                 }
                             })
-                            .catch(err => {});
+                            .catch((err) => {});
                     }
                 })
-                .catch(err => {});
+                .catch((err) => {});
         },
         openDialog(row) {
             this.dialogVisible = true;
-            this.$nextTick(_ => {
+            this.$nextTick((_) => {
                 this.$refs["add-vp-from-af-form"].init(row);
             });
         },
         submitForm(formName) {
-            this.$refs[formName].validate(formData => {
+            this.$refs[formName].validate((formData) => {
                 if (formData) {
                     const url = "/switch_svp?form=svp_af_create",
                         data = {
@@ -115,22 +140,22 @@ export default {
                                 svp_af_id: formData.svp_af_id,
                                 new_svlan: formData.new_svlan,
                                 tag_action: formData.tag_action,
-                                inner_vlan: formData.inner_vlan || 0
-                            }
+                                inner_vlan: formData.inner_vlan || 0,
+                            },
                         };
                     this.postData(url, data)
-                        .then(_ => {
+                        .then((_) => {
                             this.getData();
                             this.dialogVisible = false;
                         })
-                        .catch(_ => {});
+                        .catch((_) => {});
                 }
             });
         },
         refreshData() {
             debounce(this.getData, 1000, this);
-        }
-    }
+        },
+    },
 };
 </script>
 
