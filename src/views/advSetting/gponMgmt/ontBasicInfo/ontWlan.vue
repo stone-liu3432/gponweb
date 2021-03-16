996e1b4f3b54f7969191bf8a23af1fcddcae6e2f
stone-liu3432 <liushuang3432@163.com>
2021-03-16 17:09:04 +0800

    添加ont wlan连接配置

---
+++ src/views/advSetting/gponMgmt/ontBasicInfo/ontWlan.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>{{ data }}</div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { ENCRYPT_MAP } from "@/utils/commonData";
+export default {
+    name: "ontWlan",
+    computed: {
+        ...mapGetters(["$lang"]),
+    },
+    props: {
+        identifier: {
+            type: Number,
+        },
+        data: {
+            type: Object,
+        },
+    },
+    data() {
+        return {
+            ENCRYPT_MAP,
+            WLAN_TYPE_MAP: { 1: "2.4G", 2: "5G", 3: "All" },
+        };
+    },
+    methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+</style>
